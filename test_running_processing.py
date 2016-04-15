2f8e13731329677870b434376d0e27e0b360dbab
Magnus Nord <Magnus.Nord@glasgow.ac.uk>
2016-04-15 17:46:53 +0100

    More rewritting of old code. Move functions into different files
    Move things connected to doing the radial integration into separate
    file, which runs all the calibrations and saves the radially
    integrated signal into a HyperSpy spectrum file. Make new file with
    functions connected to modelling the Laue Zone peaks.

--- test_running_processing.py
+++ test_running_processing.py
@@ -1,5 +1,10 @@
-import laue_zone_processing_tools as lzpt
+import stem_diffraction_radial_integration as sdri
+import laue_zone_processing as lzp
+import hyperspy.api as hs
 
-s_fpd = lzpt.load_fpd_dataset("default1.hdf5")
-s_radial = lzpt.get_radial_profile_signal(s_fpd)
+#sdri.save_fpd_dataset_as_radial_profile_signal("default1.hdf5")
 
+s_radial = hs.load("default1_radial.hdf5")
+s_lfo = s_radial.isig[47.:78.]
+m_lfo_one_gaussian = lzp.model_lfo_with_one_gaussian(s_lfo)
+m_lfo_two_gaussian = lzp.model_lfo_with_two_gaussians(s_lfo)
