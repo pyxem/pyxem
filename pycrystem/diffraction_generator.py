554276b94f42132ebf3ea9a9fca420a3fa512fd9
dnjohnstone <duncanjohnstone@live.co.uk>
2016-09-16 16:23:43 +0100

    utils for sims and expts

--- pycrystem/diffraction_generator.py
+++ pycrystem/diffraction_generator.py
@@ -2,45 +2,6 @@ from scipy.constants import h, m_e, e, c, pi
 import math
 import numpy as np
 
-def get_electron_wavelength(accelerating_voltage):
-    """Calculates the (relativistic) electron wavelength in Angstroms
-    for a given accelerating voltage in kV.
-
-    Evaluates
-
-    Parameters
-    ----------
-    accelerating_voltage : float
-        The accelerating voltage in kV.
-
-    Returns
-    -------
-    wavelength : float
-        The relativistic electron wavelength in Angstroms.
-
-    """
-    E = accelerating_voltage*1e3
-    wavelength = h / math.sqrt(2 * m_e * e * E *\
-                               (1 + (e / (2 * m_e * c * c)) * E))*1e10
-    return wavelength
-
-def get_structure_factors(fractional_coordinates, structure):
-    """Get structure factors
-
-    Patterns
-    --------
-    fractional_coordinates :
-
-    structure
-
-    Returns
-    -------
-    structure_factors :
-
-    """
-    return np.absolute(np.sum([atom.number*np.exp(2*1j*np.pi*np.dot(fractional_coordinates, position)) for position, atom in zip(structure.frac_coords, structure.species)], axis=0))**2
-
-
 """
 This module implements an Electron Diffraction pattern calculator.
 """
