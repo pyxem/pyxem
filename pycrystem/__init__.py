d1edc1e22f50ad039718a2b3e848caddb29a0d70
bm424 <bm424@cam.ac.uk>
2016-09-20 14:50:58 +0100

    Include more features in the namespace

--- pycrystem/__init__.py
+++ pycrystem/__init__.py
@@ -1,6 +1,9 @@
 from hyperspy.api import load as hsload
 
+from pymatgen import Lattice, Structure
+
 from .diffraction_signal import ElectronDiffraction
+from .diffraction_generator import ElectronDiffractionCalculator
 
 __version__ = '0.2'
 
