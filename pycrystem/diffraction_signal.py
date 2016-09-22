b22b3cd2195688e982dd47d0f700add3beecb1f1
dnjohnstone <duncanjohnstone@live.co.uk>
2016-09-22 18:36:36 +0100

    add line profile method

--- pycrystem/diffraction_signal.py
+++ pycrystem/diffraction_signal.py
@@ -137,6 +137,14 @@ class ElectronDiffraction(Signal2D):
         ap.add_widget(self, axes=self.axes_manager.signal_axes, color='red')
         ap.interactive(self, navigation_signal='same').plot()
 
+    def plot_line_profile(self, x1, y1, x2, y2):
+        """Plots an interactive line profile.
+        """
+        self.plot()
+        ap = roi.Line2DROI(x1=x1, y1=y1, x2=x2, y2=y2)
+        ap.add_widget(self, axes=self.axes_manager.signal_axes, color='red')
+        ap.interactive(self, navigation_signal='same').plot()
+
     def get_direct_beam_mask(self, radius=None, center=None):
         """Generate a signal mask for the direct beam.
 
