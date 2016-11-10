e107ef777d62365853ba7fabc16242948f317576
dnjohnstone <duncanjohnstone@live.co.uk>
2016-11-10 00:01:56 +0000

    outline tests

---
+++ tests/test_tensor_field2d.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 The PyCrystEM developers
+#
+# This file is part of PyCrystEM.
+#
+# PyCrystEM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyCrystEM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyCrystEM.  If not, see <http://www.gnu.org/licenses/>.
+import numpy as np
+import nose.tools as nt
+
+from pycrystem.tensor_field2d import TensorField2D
+#from hyperspy.signals import Signal1D, Signal2D
+
+
+class TestTensorField2D:
+
+    def setUp(self):
+        # Create an empty diffraction pattern
+        tf = TensorField2D(np.ones((2, 2, 3, 3)))
+        self.signal = tf
+
+    def test_default_param(self):
+        dp = self.signal
+        md = dp.metadata
+        nt.assert_equal(md.Acquisition_instrument.TEM.rocking_angle,
+                        preferences.ElectronDiffraction.ed_precession_angle)
