a00153eea7a91f13197a0c58ac1a68890a6efab7
Tomas Ostasevicius <t.ostasevicius@gmail.com>
2017-04-05 15:32:35 +0100

    fix atomic scattering paths

--- pycrystem/utils/sim_utils.py
+++ pycrystem/utils/sim_utils.py
@@ -27,7 +27,10 @@ from scipy.constants import h, m_e, e, c, pi
 import os
 import json
 
-with open("pycrystem/utils/atomic_scattering_params.json") as f:
+ATOMIC_SCATTERING_PATH = os.path.join(os.path.dirname(__file__),
+                                      "atomic_scattering_params.json")
+
+with open(ATOMIC_SCATTERING_PATH) as f:
     ATOMIC_SCATTERING_PARAMS = json.load(f)
 
 
