55bb3d15db430914264ef2da662a24c104a336d5
dnjohnstone <duncanjohnstone@live.co.uk>
2017-07-13 12:24:34 +0100

    typo correction

--- pycrystem/diffraction_signal.py
+++ pycrystem/diffraction_signal.py
@@ -425,7 +425,7 @@ class ElectronDiffraction(Signal2D):
             self.data = self.data / scale
             denoised = self.map(regional_filter, inplace=False, *args, **kwargs)
             denoised.map(filters.rank.mean, selem=square(3))
-            demoised.data = denoised.data / denoised.data.max() * scale
+            denoised.data = denoised.data / denoised.data.max() * scale
 
         if method=='model':
             bg = self.get_background_model(*args, **kwargs)
