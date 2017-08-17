19cd6d53fd1545fbfe84ba1d6ecd3bb4ee104aaf
Magnus Nord <Magnus.Nord@glasgow.ac.uk>
2017-08-17 20:21:53 +0100

    Add io_tools: for loading hdf5-files converted Glasgow script

---
+++ fpd_data_processing/io_tools.py
@@ -0,0 +1,48 @@
+import h5py
+import logging
+from hyperspy.io_plugins import emd
+from hyperspy.io import load_with_reader
+from fpd_data_processing.pixelated_stem_class import PixelatedSTEM
+
+
+def _fpd_checker(filename, attr_substring='fpd_version'):
+    hdf5_file = h5py.File(filename)
+    for attr in hdf5_file.attrs:
+        if attr_substring in attr:
+            return(True)
+    return(False)
+
+
+def load_fpd_dataset(filename):
+    if not _fpd_checker(filename):
+        raise Exception("fpd_version header not found")
+    logging.basicConfig(level=logging.ERROR)
+    s_list = load_with_reader(filename, reader=emd)
+    logging.basicConfig(level=logging.WARNING)
+    temp_s = None
+    longest_dims = 0
+    for s in s_list:
+        if len(s.data.shape) > longest_dims:
+            longest_dims = len(s.data.shape)
+    if longest_dims < 4:
+        raise ValueError("Pixelated dataset not found")
+    for s in s_list:
+        if len(s.data.shape) == longest_dims:
+            temp_s = s
+            break
+    if longest_dims == 4:
+        s = temp_s.transpose(signal_axes=(0, 1))
+    elif longest_dims == 5:
+        s = temp_s.isig[:,:,0,:,:].transpose(signal_axes=(0, 1))
+    else:
+        raise Exception(
+                "Pixelated dataset not found")
+    s_new = PixelatedSTEM(s.data)
+    for i in range(len(s.axes_manager.shape)):
+        s_new.axes_manager[i].offset = s.axes_manager[i].offset
+        s_new.axes_manager[i].scale = s.axes_manager[i].scale
+        s_new.axes_manager[i].name = s.axes_manager[i].name
+        s_new.axes_manager[i].units = s.axes_manager[i].units
+
+    s_new.metadata = s.metadata.deepcopy()
+    return PixelatedSTEM(s.data)
