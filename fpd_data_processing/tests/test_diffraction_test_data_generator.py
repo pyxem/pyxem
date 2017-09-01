422e4b7538fce0e3b303645b7470f5a717ffc7eb
Ida <idahjorth@gmail.com>
2017-09-01 19:25:17 +0100

    TestData: first working unittest

--- fpd_data_processing/tests/test_diffraction_test_data_generator.py
+++ fpd_data_processing/tests/test_diffraction_test_data_generator.py
@@ -1,7 +1,9 @@
 import unittest
-import 
+import fpd_data_processing.diffraction_test_data_generator as TestData
+
 
 class test_diffraction_test_data_genrator(unittest.TestCase):
 
     def test_init(self):
-
+        test_data_1 = TestData.TestData()
+        test_data_2 = TestData.TestData(size_x=1,size_y=10,scale=0.05)
