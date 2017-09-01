3ab61679867c2dda6dbaed1025c2be49a15eb957
Stef Smeets <stef.smeets@mmk.su.se>
2017-09-01 11:35:57 +0200

    Use setuptools & move author info out of package
    Setuptools is convenient because it allows 'python setup.py develop',
    and pulling author info from package requires the whole package to
    be imported, which may cause problems (i.e. if dependencies are missing).

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from pycrystem import __version__, __author__, __email__
+from setuptools import setup
 
 
 setup(
     name='pycrystem',
-    version=__version__,
+    version="0.4",
     description='An open-source Python library for crystallographic electron'
                 'microscopy.',
-    author=__author__,
-    author_email=__email__,
+    author="Duncan Johnstone",
+    author_email="dnj23@cam.ac.uk",
     packages=[
         'pycrystem',
         'pycrystem.utils',
