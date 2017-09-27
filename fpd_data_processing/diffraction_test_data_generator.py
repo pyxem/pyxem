Bugfix centerpoint, decimal precision

fpd_data_processing/diffraction_test_data_generator.py
@@ -199,13 +199,15 @@ class TestData:
         self.xx, self.yy = np.meshgrid(self.X, self.Y, sparse=True)
 
     def add_disk(self, x0=50, y0=50, r=5, I=10):
-        self.z_list.append(Disk(self.xx, self.yy, self.scale, x0, y0, r, I))
+        scale = self.scale/self.downscale_factor
+        self.z_list.append(Disk(self.xx, self.yy, scale, x0, y0, r, I))
         self.update_signal()
 
     def add_ring(self, x0=50, y0=50, r=20, I=10, lw_pix=1):
         lw = lw_pix*self.scale
+        scale = self.scale/self.downscale_factor
         self.z_list.append(
-                Ring(self.xx, self.yy, self.scale, x0, y0, r, I, lw=lw))
+                Ring(self.xx, self.yy, scale, x0, y0, r, I, lw=lw))
         self.update_signal()
 
     def make_signal(self):

fpd_data_processing/tests/test_diffraction_test_data_generator.py
@@ -69,7 +69,8 @@ class test_diffraction_test_data_generator(unittest.TestCase):
                 size_x=100, size_y=200,
                 default=False, blur=True, downscale=False)
         test6.add_disk(50, 30, 500, 10)
-        self.assertTrue((test6.signal.data==10).all())
+        test6_ref = np.full_like(test6.signal.data, 10.)
+        np.testing.assert_allclose(test6.signal.data, test6_ref, rtol=1e-05)
 
     def test_large_disk(self):
         test_data_1 = TestData.TestData(