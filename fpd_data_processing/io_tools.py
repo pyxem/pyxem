78a497df644a32b25e48bb7efc30f225a7ccc222
Magnus Nord <Magnus.Nord@glasgow.ac.uk>
2017-10-04 19:18:04 +0100

    Add support for loading of PixelatedSTEM from any dataset HS can load

--- fpd_data_processing/io_tools.py
+++ fpd_data_processing/io_tools.py
@@ -10,19 +10,21 @@ from fpd_data_processing.pixelated_stem_class import (
 
 
 def _fpd_checker(filename, attr_substring='fpd_version'):
-    hdf5_file = h5py.File(filename)
-    for attr in hdf5_file.attrs:
-        if attr_substring in attr:
-            return(True)
+    if h5py.is_hdf5(filename):
+        hdf5_file = h5py.File(filename)
+        for attr in hdf5_file.attrs:
+            if attr_substring in attr:
+                return(True)
     return(False)
 
 
 def _hspy_checker(filename, attr_substring='fpd_version'):
-    hdf5_file = h5py.File(filename)
-    for attr in hdf5_file.attrs:
-        if 'file_format' in attr:
-            if hdf5_file.attrs['file_format'] == 'HyperSpy':
-                return(True)
+    if h5py.is_hdf5(filename):
+        hdf5_file = h5py.File(filename)
+        for attr in hdf5_file.attrs:
+            if 'file_format' in attr:
+                if hdf5_file.attrs['file_format'] == 'HyperSpy':
+                    return(True)
     return(False)
 
 
@@ -73,6 +75,17 @@ def _load_fpd_emd_file(filename):
         raise Exception(
                 "Pixelated dataset not found")
 
+    s_new = _signal2d_to_pixelated_stem(s)
+    return(s_new)
+
+
+def _load_other_file(filename, lazy=False):
+    s = load(filename, lazy=lazy)
+    s_new = _signal2d_to_pixelated_stem(s)
+    return s_new
+
+
+def _signal2d_to_pixelated_stem(s):
     s_new = PixelatedSTEM(s.data)
     for i in range(len(s.axes_manager.shape)):
         s_new.axes_manager[i].offset = s.axes_manager[i].offset
@@ -80,7 +93,7 @@ def _load_fpd_emd_file(filename):
         s_new.axes_manager[i].name = s.axes_manager[i].name
         s_new.axes_manager[i].units = s.axes_manager[i].units
     s_new.metadata = s.metadata.deepcopy()
-    return(s_new)
+    return s_new
 
 
 def load_fpd_signal(filename, lazy=False, chunk_size=(16, 16)):
@@ -100,9 +113,10 @@ def load_fpd_signal(filename, lazy=False, chunk_size=(16, 16)):
         else:
             s = _load_fpd_emd_file(filename)
     elif _hspy_checker(filename, attr_substring='HyperSpy'):
-        s = load(filename, lazy=lazy)
+        s = _load_other_file(filename, lazy=lazy)
     else:
-        raise IOError("File " + str(filename) + " not recognised")
+        # Attempt to load non-fpd and non-HyperSpy signal
+        s = _load_other_file(filename, lazy=lazy)
     return s
 
 
