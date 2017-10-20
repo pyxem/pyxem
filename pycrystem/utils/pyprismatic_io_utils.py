20dbaa06157d0d19e4f7b0c32eae2e06b8c148a4
Phillip <pc494@cam.ac.uk>
2017-10-20 18:14:11 +0100

    Producing correct output when combined with a shell script

--- pycrystem/utils/pyprismatic_io_utils.py
+++ pycrystem/utils/pyprismatic_io_utils.py
@@ -6,26 +6,32 @@ def generate_pyprismatic_input(unit_cell_structure,scaling_for_supercell=1,comme
 
         Args:
 
-        unit_cell_structure:
+        unit_cell_structure: pymatgen.Structure object
 
-        scaling: 
-
-        comment:str
+        scaling: int or (3,1) 
+            
+        Goes into pymatgen's 'make_supercell' function, can be int or (3,1) array,
+        refer to their docs for details    
+            
+        comment: str
+        
+        Be away that this cannot wrap to the next line, and should not contain '\n' characters
 
         """
-
         lattice_params = unit_cell_structure.lattice.abc
-        #now scale up
-        #TODO figure out a better code layout for this
-        unit_cell_structure.make_supercell(scaling_for_supercell)
+        
         line_1 = comment
         line_2 = lattice_params
 
+        #TODO figure out a better code layout for this
+        unit_cell_structure.make_supercell(scaling_for_supercell)
+        
         # TODO vectorise this
         Z_list = []
         for element in unit_cell_structure.species:
-                Z_list.append(element.Z)
+                Z_list.append(int(element.Z))
 
+        # TODO Get dw from Element's names
         dw = 0.076
         atomic_numbers = np.asarray(Z_list).reshape(len(unit_cell_structure.species),1)
         cart_coords = unit_cell_structure.cart_coords
@@ -35,7 +41,8 @@ def generate_pyprismatic_input(unit_cell_structure,scaling_for_supercell=1,comme
         printing_array = np.hstack([atomic_numbers,cart_coords,percent_occupied,debeye_waller])
 
         print(line_1)
-        print(line_2)
-        print(printing_array)
+        print('    {0:.3g}   {1:.3g}   {2:.3g}'.format(line_2[0],line_2[1],line_2[2]))
+        for row in printing_array:
+                print('{0:.3g} {1:.4f} {2:.4f} {3:.4f} {4:.3f} {5:.3f}'.format(row[0],row[1],row[2],row[3],row[4],row[5]))
         print("-1")
-	return None
+        return None
