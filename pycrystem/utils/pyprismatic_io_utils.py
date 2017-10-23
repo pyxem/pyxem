2d4ccbc48134fc7e5299ffbef8e7df4d73ab4b66
Phillip <pc494@cam.ac.uk>
2017-10-23 12:22:30 +0100

    Turn prints into file outputs

--- pycrystem/utils/pyprismatic_io_utils.py
+++ pycrystem/utils/pyprismatic_io_utils.py
@@ -1,41 +1,53 @@
 import numpy as np
 import pymatgen as pmg
+import os
 
-def generate_pyprismatic_input(structure,comment="Default Comment"):
+def generate_pyprismatic_input(structure,destination_path,comment="Default Comment"):
         """ Generates a .xyz file on which Pyprismatic can run
 
         Args:
 
         structure: pymatgen.Structure object 
         The entire structure
-
+        
+        destination_path: str
+        File to be created, must have .XYZ as the suffix
+            
         comment: str
         
         """
-        lattice_params = structure.lattice.abc
+        
+        if destination_path[-4:] != '.xyz' and destination_path[-4:] != '.XYZ':
+            raise IOError('File suffix is not .xyz')
+        
+        if os.path.exists(destination_path):
+            raise IOError('The file you have specified already exists')
         
         line_1 = comment
-        line_2 = lattice_params #This need a little bit of thoughts
-        
+        line_2 = structure.lattice.abc  #This need a little bit of thoughts
         
         Z_list = []
         for element in structure.species:
                 Z_list.append(int(element.Z))
 
+        atomic_numbers = np.asarray(Z_list).reshape(len(structure.species),1)
+        cart_coords = structure.cart_coords
+        percent_occupied  = np.ones_like(atomic_numbers)
+        
         # TODO Get dw from Element's names
         if True:
             dw = 0
     
-        atomic_numbers = np.asarray(Z_list).reshape(len(structure.species),1)
-        cart_coords = structure.cart_coords
-        percent_occupied  = np.ones_like(atomic_numbers)
         debeye_waller = dw*np.ones_like(atomic_numbers)
         
         printing_array = np.hstack([atomic_numbers,cart_coords,percent_occupied,debeye_waller])
-
-        print(line_1)
-        print('    {0:.3g}   {1:.3g}   {2:.3g}'.format(line_2[0],line_2[1],line_2[2]))
-        for row in printing_array:
-                print('{0:.3g} {1:.4f} {2:.4f} {3:.4f} {4:.3f} {5:.3f}'.format(row[0],row[1],row[2],row[3],row[4],row[5]))
-        print("-1")
+        
+        with open(destination_path, 'a') as f:
+            print(line_1,file=f)
+            print('    {0:.3g}   {1:.3g}   {2:.3g}'.format(line_2[0],line_2[1],line_2[2]),file=f)
+            for row in printing_array:
+                    print('{0:.3g} {1:.4f} {2:.4f} {3:.4f} {4:.3f} {5:.3f}'.format(
+                            row[0],row[1],row[2],row[3],row[4],row[5]),
+                        file=f)
+            print("-1",file=f)
         return None
