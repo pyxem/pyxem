8313976c3ebc699c639acbece4f5524a188fe51a
Phillip <pc494@cam.ac.uk>
2017-10-23 10:54:13 +0100

    Making input a supercell

--- pycrystem/utils/pyprismatic_io_utils.py
+++ pycrystem/utils/pyprismatic_io_utils.py
@@ -1,43 +1,36 @@
 import numpy as np
 import pymatgen as pmg
 
-def generate_pyprismatic_input(unit_cell_structure,scaling_for_supercell=1,comment="Default Comment"):
+def generate_pyprismatic_input(structure,comment="Default Comment"):
         """ Generates a .xyz file on which Pyprismatic can run
 
         Args:
 
-        unit_cell_structure: pymatgen.Structure object
+        structure: pymatgen.Structure object 
+        The entire structure
 
-        scaling: int or (3,1) 
-            
-        Goes into pymatgen's 'make_supercell' function, can be int or (3,1) array,
-        refer to their docs for details    
-            
         comment: str
         
-        Be away that this cannot wrap to the next line, and should not contain '\n' characters
-
         """
-        lattice_params = unit_cell_structure.lattice.abc
+        lattice_params = structure.lattice.abc
         
         line_1 = comment
-        line_2 = lattice_params
-
-        #TODO figure out a better code layout for this
-        unit_cell_structure.make_supercell(scaling_for_supercell)
+        line_2 = lattice_params #This need a little bit of thoughts
         
-        # TODO vectorise this
+        
         Z_list = []
-        for element in unit_cell_structure.species:
+        for element in structure.species:
                 Z_list.append(int(element.Z))
 
         # TODO Get dw from Element's names
-        dw = 0.076
-        atomic_numbers = np.asarray(Z_list).reshape(len(unit_cell_structure.species),1)
-        cart_coords = unit_cell_structure.cart_coords
+        if True:
+            dw = 0
+    
+        atomic_numbers = np.asarray(Z_list).reshape(len(structure.species),1)
+        cart_coords = structure.cart_coords
         percent_occupied  = np.ones_like(atomic_numbers)
         debeye_waller = dw*np.ones_like(atomic_numbers)
-        # combines the rest of what needs to come out
+        
         printing_array = np.hstack([atomic_numbers,cart_coords,percent_occupied,debeye_waller])
 
         print(line_1)
