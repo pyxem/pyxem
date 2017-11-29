82465dbeb918d92b8c5f0c187e5575c32c155a69
Phillip <pc494@cam.ac.uk>
2017-11-29 13:30:59 +0000

    Remove incorrect lines of docstring

--- pycrystem/diffraction_signal.py
+++ pycrystem/diffraction_signal.py
@@ -694,8 +694,6 @@ class ElectronDiffraction(Signal2D):
             * 'zaefferer' - based on gradient thresholding and refinement
               by local region of interest optimisation
             * 'stat' - statistical approach requiring no free params.
-            * 'massiel' - finds peaks in each direction and compares the
-              positions where these coincide.
             * 'laplacian_of_gaussians' - a blob finder implemented in
               `scikit-image` which uses the laplacian of Gaussian matrices
               approach.
