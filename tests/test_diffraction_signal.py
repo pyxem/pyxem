7f58732e0fbccbff6275f3ac2c2828677108b23e
bm424 <bm424@cam.ac.uk>
2018-01-08 14:49:14 +0000

    Continue overhall of diffraction_signal tests

--- tests/test_diffraction_signal.py
+++ tests/test_diffraction_signal.py
@@ -23,62 +23,92 @@ from pycrystem.diffraction_signal import ElectronDiffraction
 from hyperspy.signals import Signal1D, Signal2D
 
 
-@pytest.fixture
-def electron_diffraction(request):
+@pytest.fixture(params=[
+    np.array([[[0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 1., 0., 0., 0., 0.],
+               [0., 0., 1., 2., 1., 0., 0., 0.],
+               [0., 0., 0., 1., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.]],
+              [[0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 1., 0., 0., 0.],
+               [0., 0., 0., 1., 2., 1., 0., 0.],
+               [0., 0., 0., 0., 1., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.]],
+              [[0., 0., 0., 0., 0., 0., 0., 2.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 1., 0., 0., 0., 0.],
+               [0., 0., 1., 2., 1., 0., 0., 0.],
+               [0., 0., 0., 1., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.]],
+              [[0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 2., 0., 0., 0.],
+               [0., 0., 0., 2., 2., 2., 0., 0.],
+               [0., 0., 0., 0., 2., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.]]]),
+])
+def diffraction_pattern(request):
     return ElectronDiffraction(request.param)
 
 
-@pytest.mark.parametrize('electron_diffraction', [
-    (np.ones((2, 2, 2, 2)),),
-], indirect=['electron_diffraction'])
-def test_default_params(electron_diffraction):
-    # TODO: preferences in pycrystem
-    a = electron_diffraction.metadata.Acquisition_instrument.TEM.rocking_angle
+@pytest.mark.skip(reason='Defaults not implemented in PyCrystEM')
+def test_default_params(diffraction_pattern):
+    a = diffraction_pattern.metadata.Acquisition_instrument.TEM.rocking_angle
     pass
 
 
-@pytest.mark.incremental
+@pytest.mark.parametrize('calibration, center', [
+    (1, (4, 4),),
+    (0.017, (3, 3)),
+    (0.5, None,),
+])
+def test_set_calibration(diffraction_pattern, calibration, center):
+    diffraction_pattern.set_calibration(calibration, center=center)
+    dx, dy = diffraction_pattern.axes_manager.signal_axes
+    assert dx.scale == calibration and dy.scale == calibration
+    if center is not None:
+        assert np.all(diffraction_pattern.isig[0., 0.].data == diffraction_pattern.isig[center[0], center[1]].data)
+
+
+@pytest.mark.parametrize('dark_reference, bright_reference', [
+    (-1, 1),
+    (0, 1),
+    (0, 256),
+])
+def test_apply_gain_normalisation(diffraction_pattern: ElectronDiffraction,
+                                  dark_reference, bright_reference):
+    diffraction_pattern.apply_gain_normalisation(
+        dark_reference=dark_reference, bright_reference=bright_reference)
+    assert diffraction_pattern.max() == bright_reference
+    assert diffraction_pattern.min() == dark_reference
+
+
+def test_reproject_as_polar(diffraction_pattern: ElectronDiffraction):
+    shape_cartesian = diffraction_pattern.axes_manager.signal_shape
+    diffraction_pattern.reproject_as_polar()
+    assert isinstance(diffraction_pattern, Signal2D)
+    shape_polar = diffraction_pattern.axes_manager.signal_shape
+    assert shape_polar[0] == max(shape_cartesian)
+    assert shape_polar[1] > np.sqrt(2) * shape_cartesian[0] / 2
+
+
+def test_get_diffraction_variance(diffraction_pattern: ElectronDiffraction):
+    dv = diffraction_pattern.get_diffraction_variance()
+    assert dv.axes_manager.navigation_shape == (3,)
+    assert dv.axes_manager.signal_shape == diffraction_pattern.axes_manager.signal_shape
+
+
 class TestDirectBeamMethods:
-
-    @pytest.fixture
-    def diffraction_pattern(self):
-        dp = ElectronDiffraction(np.zeros((4, 8, 8)))
-        dp.data[0]= np.array([[0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 1., 0., 0., 0., 0.],
-                              [0., 0., 1., 2., 1., 0., 0., 0.],
-                              [0., 0., 0., 1., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.]])
-
-        dp.data[1]= np.array([[0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 1., 0., 0., 0.],
-                              [0., 0., 0., 1., 2., 1., 0., 0.],
-                              [0., 0., 0., 0., 1., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.]])
-
-        dp.data[2]= np.array([[0., 0., 0., 0., 0., 0., 0., 2.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 1., 0., 0., 0., 0.],
-                              [0., 0., 1., 2., 1., 0., 0., 0.],
-                              [0., 0., 0., 1., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.]])
-
-        dp.data[3]= np.array([[0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 2., 0., 0., 0.],
-                              [0., 0., 0., 2., 2., 2., 0., 0.],
-                              [0., 0., 0., 0., 2., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 0., 0., 0., 0., 0.]])
-        return dp
 
     def test_get_direct_beam_position(self, diffraction_pattern):
         c = diffraction_pattern.get_direct_beam_position(radius=2)
@@ -138,97 +168,116 @@ class TestDirectBeamMethods:
         assert np.allclose(mask_calculated, mask_expected)
 
 
-class Test_radial_profile:
+class TestRadialProfile:
 
-    def setUp(self):
+    @pytest.fixture
+    def diffraction_pattern(self):
         dp = ElectronDiffraction(np.zeros((2, 8, 8)))
-        dp.data[0]= np.array([[0., 0., 1., 2., 2., 1., 0., 0.],
-                              [0., 1., 2., 3., 3., 2., 1., 0.],
-                              [1., 2., 3., 4., 4., 3., 2., 1.],
-                              [2., 3., 4., 5., 5., 4., 3., 2.],
-                              [2., 3., 4., 5., 5., 4., 3., 2.],
-                              [1., 2., 3., 4., 4., 3., 2., 1.],
-                              [0., 1., 2., 3., 3., 2., 1., 0.],
-                              [0., 0., 1., 2., 2., 1., 0., 0.]])
+        dp.data[0] = np.array([[0., 0., 1., 2., 2., 1., 0., 0.],
+                               [0., 1., 2., 3., 3., 2., 1., 0.],
+                               [1., 2., 3., 4., 4., 3., 2., 1.],
+                               [2., 3., 4., 5., 5., 4., 3., 2.],
+                               [2., 3., 4., 5., 5., 4., 3., 2.],
+                               [1., 2., 3., 4., 4., 3., 2., 1.],
+                               [0., 1., 2., 3., 3., 2., 1., 0.],
+                               [0., 0., 1., 2., 2., 1., 0., 0.]])
 
-        dp.data[1]= np.array([[0., 1., 2., 3., 3., 3., 2., 1.],
-                              [1., 2., 3., 4., 4., 4., 3., 2.],
-                              [2., 3., 4., 5., 5., 5., 4., 3.],
-                              [2., 3., 4., 5., 6., 5., 4., 3.],
-                              [2., 3., 4., 5., 5., 5., 4., 3.],
-                              [1., 2., 3., 4., 4., 4., 3., 2.],
-                              [0., 1., 2., 3., 3., 3., 2., 1.],
-                              [0., 0., 1., 2., 2., 2., 1., 0.]])
-        self.signal = dp
+        dp.data[1] = np.array([[0., 1., 2., 3., 3., 3., 2., 1.],
+                               [1., 2., 3., 4., 4., 4., 3., 2.],
+                               [2., 3., 4., 5., 5., 5., 4., 3.],
+                               [2., 3., 4., 5., 6., 5., 4., 3.],
+                               [2., 3., 4., 5., 5., 5., 4., 3.],
+                               [1., 2., 3., 4., 4., 4., 3., 2.],
+                               [0., 1., 2., 3., 3., 3., 2., 1.],
+                               [0., 0., 1., 2., 2., 2., 1., 0.]])
+        return dp
 
-    def test_radial_profile_signal_type(self):
-        dp=self.signal
-        rp = dp.get_radial_profile()
-        nt.assert_true(isinstance(rp, Signal1D))
+    def test_radial_profile_signal_type(self, diffraction_pattern):
+        rp = diffraction_pattern.get_radial_profile()
+        assert isinstance(rp, Signal1D)
 
-    def test_radial_profile_no_centers(self):
-        dp = self.signal
-        rp = dp.get_radial_profile()
-        np.testing.assert_allclose(rp.data, np.array([[5., 4.25, 2.875,
-                                                       1.7, 0.92857143, 0.],
-                                                      [5., 4.75, 3.625,
-                                                       2.5, 1.71428571,0.6]]),
-                                                      atol=1e-3)
-
-    def test_radial_profile_with_centers(self):
-        dp = self.signal
-        rp = dp.get_radial_profile(centers=np.array([[4, 3], [4, 3]]))
-        np.testing.assert_allclose(rp.data, np.array([[5., 4.25, 2.875,
-                                                       1.7, 0.92857143, 0.],
-                                                      [5., 4.375, 3.5,
-                                                       2.4, 2.07142857, 1.]]),
-                                                      atol=1e-3)
+    @pytest.mark.parametrize('centers, expected', [
+        (None, np.array([
+            [[5., 4.25, 3.16666667, 2.125, 0.95454545, 0., 0.],
+             [5., 4.375, 3.66666667, 3., 2.33333333, 1.375, 0.5]]
+        ])),
+        (np.array([[4, 3], [4, 3]]), np.array([
+            [[5., 4.25, 3.16666667, 2.125, 0.95454545, 0., 0.],
+             [5., 4.375, 3.66666667, 2.75, 2.04545455, 1., 1.]]
+        ])),
+    ])
+    def test_radial_profile(self, diffraction_pattern, centers, expected):
+        rp = diffraction_pattern.get_radial_profile(centers=centers)
+        assert np.allclose(rp.data, expected, atol=1e-3)
 
 
-class Test_correct_geometric_distortion:
+class TestApplyAffineTransformation:
 
-    def setUp(self):
-        dp = ElectronDiffraction(np.zeros((2, 8, 8)))
-        dp.data[0]= np.array([[0., 0., 1., 2., 2., 1., 0., 0.],
-                              [0., 1., 2., 3., 3., 2., 1., 0.],
-                              [1., 2., 3., 4., 4., 3., 2., 1.],
-                              [2., 3., 4., 5., 5., 4., 3., 2.],
-                              [2., 3., 4., 5., 5., 4., 3., 2.],
-                              [1., 2., 3., 4., 4., 3., 2., 1.],
-                              [0., 1., 2., 3., 3., 2., 1., 0.],
-                              [0., 0., 1., 2., 2., 1., 0., 0.]])
+    def test_affine_transformation_signal_type(self, diffraction_pattern):
+        diffraction_pattern.apply_affine_transformation(
+            D=np.array([[1., 0., 0.],
+                        [0., 1., 0.],
+                        [0., 0., 1.]]))
+        assert isinstance(diffraction_pattern, ElectronDiffraction)
 
-        dp.data[1]= np.array([[0., 1., 2., 3., 3., 3., 2., 1.],
-                              [1., 2., 3., 4., 4., 4., 3., 2.],
-                              [2., 3., 4., 5., 5., 5., 4., 3.],
-                              [2., 3., 4., 5., 6., 5., 4., 3.],
-                              [2., 3., 4., 5., 5., 5., 4., 3.],
-                              [1., 2., 3., 4., 4., 4., 3., 2.],
-                              [0., 1., 2., 3., 3., 3., 2., 1.],
-                              [0., 0., 1., 2., 2., 2., 1., 0.]])
-        self.signal = dp
+    @pytest.mark.parametrize('diffraction_pattern, transformation, expected', [
+        (
+            np.array([
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0],
+            ], dtype=float), np.array([
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+            ], dtype=float), np.array([
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0]
+            ], dtype=float),
+        ),
+        (
+            np.array([
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0],
+            ], dtype=float),
+            np.array([
+                [0, 1, 0],
+                [1, 0, 0],
+                [0, 0, 1],
+            ], dtype=float),
+            np.array([
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 0, 0],
+            ], dtype=float),
+        )
+    ], indirect=['diffraction_pattern'])
+    def test_geometric_distortion(self, diffraction_pattern, transformation, expected):
+        diffraction_pattern.apply_affine_transformation(D=transformation)
+        assert np.allclose(diffraction_pattern.data, expected)
 
-    def test_correct_geometric_distortion_signal_type(self):
-        dp=self.signal
-        dp.correct_geometric_distortion(D=np.array([[1., 0., 0.],
-                                                    [0., 1., 0.],
-                                                    [0., 0., 1.]]))
-        nt.assert_true(isinstance(dp, ElectronDiffraction))
 
-#    def test_geometric_distortion_rotation_origin(self):
-#        dp = self.signal
-#        dp.correct_geometric_distortion()
-#        np.testing.assert_allclose(rp.data, np.array([[5., 4.25, 2.875,
-#                                                       1.7, 0.92857143, 0.],
-#                                                      [5., 4.75, 3.625,
-#                                                       2.5, 1.71428571,0.6]]),
-#                                                      atol=1e-3)
+class TestBackgroundMethods:
 
-#    def test_geometric_distortion(self):
-#        dp = self.signal
-#        rp = dp.get_radial_profile(centers=np.array([[4, 3], [4, 3]]))
-#        np.testing.assert_allclose(rp.data, np.array([[5., 4.25, 2.875,
-#                                                       1.7, 0.92857143, 0.],
-#                                                      [5., 4.375, 3.5,
-#                                                       2.4, 2.07142857, 1.]]),
-#                                                      atol=1e-3)
+    @pytest.mark.parametrize('saturation_radius', [
+        1,
+        2,
+    ])
+    def test_get_background_model(
+            self, diffraction_pattern: ElectronDiffraction, saturation_radius):
+        bgm = diffraction_pattern.get_background_model(saturation_radius)
+        assert bgm.axes_manager.signal_shape == diffraction_pattern.axes_manager.signal_shape
+
+    @pytest.mark.parametrize('method, kwargs', [
+        ('h-dome', {'h': 1}),
+        ('model', {'saturation_radius': 2, }),
+        ('gaussian_difference', {'sigma_min': 0.5, 'sigma_max': 1, }),
+        ('median', {'footprint': 4, })
+    ])
+    def test_remove_background(self, diffraction_pattern: ElectronDiffraction,
+                               method, kwargs):
+        bgr = diffraction_pattern.remove_background(method=method, **kwargs)
+        assert bgr.data.shape == diffraction_pattern.data.shape
+        assert bgr.max() <= diffraction_pattern.max()
