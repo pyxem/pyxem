0e60782b8e638170501aa66d98714229e12b406a
Phillip <pc494@cam.ac.uk>
2018-01-16 12:39:28 +0000

    Add comments, reorder and respace code for clarity

--- pyxem/utils/pyprismatic_io_utils.py
+++ pyxem/utils/pyprismatic_io_utils.py
@@ -5,40 +5,39 @@ import pyprismatic as pr
 
 def generate_pyprismatic_input(structure):
         """     Generates a .XYZ file on which Pyprismatic can run
+        
         Args:
             structure: pymatgen.Structure object 
                 The entire structure
         Returns:
-            None + file 'PP_input.XYZ'
+            None
+            file 'PP_input.XYZ'
         
         """
         
         if os.path.exists('PP_input.XYZ'):
             raise IOError('The file the program is attempting to write to already exists, please remove it.')
-            #It would be addition (ie not overwrite) here
-            pass
+            #If one turned this warning off extra data would be added to the existing file (NOT a simple overwrite) 
         
         atomic_numbers = np.asarray([S.Z for S in structure.species]).reshape(len(structure.species),1)
         cart_coords = structure.cart_coords
         percent_occupied  = np.ones_like(atomic_numbers)
+        # TODO Get debeye-wallers from elements' names
         debeye_waller = np.zeros_like(atomic_numbers)
-        # TODO Get dw from Element's name
-        
         printing_array = np.hstack([atomic_numbers,cart_coords,percent_occupied,debeye_waller])
         
         # TODO introduce logic to allow actual unit cells to be introduced
-        
-        line_2 = [np.max(cart_coords[:,0]),np.max(cart_coords[:,1]),np.max(cart_coords[:,2])]
+        unit_cell_size = [np.max(cart_coords[:,0]),np.max(cart_coords[:,1]),np.max(cart_coords[:,2])]
         
         # TODO consider if these violations are prevented by the use of pymatgen.Structure
         if [0,0,0] not in structure.cart_coords:
             raise ValueError('To use the whole sample as a unit cell we require [0,0,0] as a co-ordinate')
-        if np.max(line_2) <= 1:
+        if np.max(unit_cell_size) <= 1:
             raise ValueError('Inputs should not be in fractional form')
         
         with open('PP_input.XYZ', 'a') as f:
             print("Default Comment",file=f)
-            print('    {0:.3g}   {1:.3g}   {2:.3g}'.format(line_2[0],line_2[1],line_2[2]),file=f)
+            print('    {0:.3g}   {1:.3g}   {2:.3g}'.format(unit_cell_size[0],unit_cell_size[1],unit_cell_size[2]),file=f)
             for row in printing_array:
                     print('{0:.3g} {1:.4f} {2:.4f} {3:.4f} {4:.3f} {5:.3f}'.format(
                             row[0],row[1],row[2],row[3],row[4],row[5]),
@@ -49,20 +48,18 @@ def generate_pyprismatic_input(structure):
 def run_pyprismatic_simulation(prismatic_kwargs=None):
     if prismatic_kwargs == None:
         prismatic_kwargs = {}
-    print(prismatic_kwargs)
     if 'filenameAtoms' not in prismatic_kwargs.keys():
         prismatic_kwargs.update({'filenameAtoms':"PP_input.XYZ"})
     if 'filenameOutput' not in prismatic_kwargs.keys():
         prismatic_kwargs.update({'filenameOutput':"PP_output.mrc"})
     print(prismatic_kwargs)
     meta = pr.Metadata(**prismatic_kwargs) ##Sticks to defaults apart from the unpacked dict
-    ## Delete input
-    ## Print the meta to a file
     meta.go()
+    # TODO Clean up here so that users don't have loads of files floating around
     return meta
 
 def import_pyprismatic_data(meta):
     read_file = meta.filenameOutput
     output = pr.fileio.readMRC(read_file)
-    ## Delete output
+    # TODO Choose and implement an output format
     return output
