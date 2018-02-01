c9d35ed7ce503d13ad20a6327e8efe170780951e
dnjohnstone <duncanjohnstone@live.co.uk>
2018-02-01 10:57:35 +0000

    diffraction vectors test outline & tidy

--- pyxem/utils/vector_utils.py
+++ pyxem/utils/vector_utils.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
 
+import numpy as np
+
 def calculate_norms(z):
     norms = []
     for i in z[0]:
@@ -23,8 +25,8 @@ def calculate_norms(z):
     return np.asarray(norms)
 
 def get_indices_from_distance_matrix(distances, distance_threshold):
-    #Checks if the distances from one vector in vlist to all other vectors in gvlist
-    #is larger than distance_threshold.
+    # Checks if the distances from one vector in vlist to all other vectors in
+    # gvlist are larger than distance_threshold.
     new_indices = []
     l = np.shape(distances)[0]
     for i in range(np.shape(distances)[1]):
---
+++ tests/test_diffraction_vectors.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2018 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+import pytest
+from hyperspy.signals import Signal1D, Signal2D
+from pyxem.signals.diffraction_vectors import DiffractionVectors
+
+
+@pytest.fixture(params=[
+    np.array([[[0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 1., 0., 0., 0., 0.],
+               [0., 0., 1., 2., 1., 0., 0., 0.],
+               [0., 0., 0., 1., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.]],
+              [[0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 1., 0., 0., 0.],
+               [0., 0., 0., 1., 2., 1., 0., 0.],
+               [0., 0., 0., 0., 1., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.]],
+              [[0., 0., 0., 0., 0., 0., 0., 2.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 1., 0., 0., 0., 0.],
+               [0., 0., 1., 2., 1., 0., 0., 0.],
+               [0., 0., 0., 1., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.]],
+              [[0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 2., 0., 0., 0.],
+               [0., 0., 0., 2., 2., 2., 0., 0.],
+               [0., 0., 0., 0., 2., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0., 0., 0., 0.]]]),
+])
+def diffraction_pattern(request):
+    return ElectronDiffraction(request.param)
+
+
+@pytest.mark.skip(reason='Defaults not implemented in PyCrystEM')
+def test_default_params(diffraction_pattern):
+    a = diffraction_pattern.metadata.Acquisition_instrument.TEM.rocking_angle
+    pass
+
+
+@pytest.mark.parametrize('calibration, center', [
+    (1, (4, 4),),
+    (0.017, (3, 3)),
+    (0.5, None,),
+])
+def test_set_calibration(diffraction_pattern, calibration, center):
+    calibrated_center = calibration * np.array(center) if center is not None else center
+    diffraction_pattern.set_calibration(calibration, center=calibrated_center)
+    dx, dy = diffraction_pattern.axes_manager.signal_axes
+    assert dx.scale == calibration and dy.scale == calibration
+    if center is not None:
+        assert np.all(diffraction_pattern.isig[0., 0.].data == diffraction_pattern.isig[center[0], center[1]].data)
