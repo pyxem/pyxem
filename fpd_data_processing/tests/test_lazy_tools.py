e751057297fcddd7f911e352c9fd0a9efdf28728
Magnus Nord <magnunor@gmail.com>
2018-02-14 21:38:03 +0000

    Add better tests for lazy_tools

--- fpd_data_processing/tests/test_lazy_tools.py
+++ fpd_data_processing/tests/test_lazy_tools.py
@@ -15,6 +15,21 @@ class TestGetDaskChunkSliceList:
         assert slice_list[2] == np.s_[5:10, 0:5, :, :]
         assert slice_list[3] == np.s_[5:10, 5:10, :, :]
 
+    def test_nav_chunk_1(self):
+        dask_array = da.zeros((2, 2, 10, 10), chunks=(1, 1, 10, 10))
+        slice_list = lt._get_dask_chunk_slice_list(dask_array)
+        assert len(slice_list) == 4
+        assert slice_list[0] == np.s_[0:1, 0:1, :, :]
+        assert slice_list[1] == np.s_[0:1, 1:2, :, :]
+        assert slice_list[2] == np.s_[1:2, 0:1, :, :]
+        assert slice_list[3] == np.s_[1:2, 1:2, :, :]
+
+    def test_nav_chunk_full(self):
+        dask_array = da.zeros((2, 2, 10, 10), chunks=(2, 2, 10, 10))
+        slice_list = lt._get_dask_chunk_slice_list(dask_array)
+        assert len(slice_list) == 1
+        assert slice_list[0] == np.s_[0:2, 0:2, :, :]
+
     def test_2_dim_error(self):
         dask_array = da.zeros((50, 50), chunks=(5, 5))
         with pytest.raises(NotImplementedError):
@@ -29,3 +44,35 @@ class TestGetDaskChunkSliceList:
         dask_array = da.zeros((5, 6, 7, 8, 9, 9), chunks=(2, 2, 2, 2, 2, 2))
         with pytest.raises(NotImplementedError):
             lt._get_dask_chunk_slice_list(dask_array)
+
+
+def sum_frame(image, multiplier=1):
+    data = image.sum()*multiplier
+    return data
+
+
+class TestCalculateFunctionOnDaskArray:
+
+    def test_simple(self):
+        dask_array = da.ones((10, 10, 50, 50), chunks=(5, 5, 25, 25))
+        data = lt._calculate_function_on_dask_array(
+                dask_array, sum_frame, show_progressbar=False)
+        assert data.shape == (10, 10)
+        assert (data == (np.ones((10, 10))*50*50)).all()
+
+    def test_nav_size(self):
+        dask_array = da.ones((6, 9, 10, 10), chunks=(3, 3, 10, 10))
+        data = lt._calculate_function_on_dask_array(
+                dask_array, sum_frame, show_progressbar=False)
+        assert data.shape == (6, 9)
+
+    def test_value(self):
+        numpy_array = np.zeros((6, 9, 10, 10))
+        numpy_array[0, 0, :, :] += 1
+        dask_array = da.from_array(numpy_array, chunks=(3, 3, 10, 10))
+        data = lt._calculate_function_on_dask_array(
+                dask_array, sum_frame, show_progressbar=False)
+        assert data.shape == (6, 9)
+        assert data[0, 0] == 100
+        data[0, 0] = 0
+        assert not data.any()
