f02b0e9519227575ed09171c2fa0ec05a48ac766
Magnus Nord <magnunor@gmail.com>
2018-02-18 15:55:11 +0000

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         packages = [
             'fpd_data_processing',
             ],
-        version = '0.2.0',
+        version = '0.2.1',
         description = 'Library for processing scanning transmission electron microscopy data acquired using a fast pixelated detector',
         author = 'Magnus Nord',
         author_email = 'magnunor@gmail.com',
@@ -21,5 +21,6 @@ setup(
             'ipython>=2.0',
             'matplotlib>=2.0',
             'hyperspy>=1.3',
+            'dask',
             ],
 )
