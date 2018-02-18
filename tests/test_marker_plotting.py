aa2bd3e274a8401edec258e95fc84d26b23e75c6
pc494 <pc494@cam.ac.uk>
2018-02-18 15:00:55 +0000

    Medium sized refactor for clarity

--- tests/test_marker_plotting.py
+++ tests/test_marker_plotting.py
@@ -28,57 +28,52 @@ When you run this the markers should land at the center of the peaks
 near the dots.
 """
 
-dps, dp_cord_list = [],[]
-for alpha in [0,1,2,3]:
-    coords = np.array([5+3*alpha,20*alpha,0]).reshape(1,3)
-    dp_cord_list.append(coords[:,:2]) #stores the simulations coords
-    back = np.zeros((144,144))
-    x = coords.astype(int)[0,0]
-    y = coords.astype(int)[0,1]
-    back[x,y] = 1 
-    dps.append(back.T) #stores a numpy array of pattern, This is dangerous
+@pytest.fixture
+def generate_dp_cord_list():
+    dp_cord_list = []
+    for alpha in [0,1,2,3]:
+        coords = np.array([5+3*alpha,20*alpha,0]).reshape(1,3)
+        dp_cord_list.append(coords[:,:2])
+    return dp_cord_list
 
-# And onwards
-dp = pxm.ElectronDiffraction(np.array([dps[0:2],dps[2:]])) #now from a 2x2 array of patterns
-peaks = hs.signals.Signal2D(np.array([dp_cord_list[0:2],dp_cord_list[2:]]))
-
-### And plot!
-
-mmx,mmy = generate_marker_inputs_from_peaks(peaks)
-dp.plot(cmap='viridis')
-for mx,my in zip(mmx,mmy):
-    m = hs.markers.point(x=mx,y=my,color='red',marker='x')
-    dp.add_marker(m,plot_marker=True,permanent=True)
+def local_plotter(dp,dp_cord_list):
+    peaks = hs.signals.Signal2D(np.array([dp_cord_list[0:2],dp_cord_list[2:]]))
+    mmx,mmy = generate_marker_inputs_from_peaks(peaks)
+    dp.plot(cmap='viridis')
+    for mx,my in zip(mmx,mmy):
+        m = hs.markers.point(x=mx,y=my,color='red',marker='x')
+        dp.add_marker(m,plot_marker=True,permanent=True)
+    
+    return None
 
 def test_marker_placement_correct_alpha():
+    dps = []
+    dp_cord_list = generate_dp_cord_list()
+    for coords in dp_cord_list:
+        back = np.zeros((144,144))
+        x = coords.astype(int)[0,0]
+        y = coords.astype(int)[0,1]
+        back[x,y] = 1 
+        dps.append(back.T) #stores a numpy array of pattern, This is dangerous (T everywhere)
+
+    dp = pxm.ElectronDiffraction(np.array([dps[0:2],dps[2:]])) 
+    local_plotter(dp,dp_cord_list)
+
     #This is human assessed, if you see this comment, you should check it
     assert True
 
 ### Now get .as_signal()
-
-dps = []
-dp_cord_list= np.divide(dp_cord_list,80)
-max_r = np.max(dp_cord_list) + 0.1
-# Create 4 random diffraction simulations
-half_side_length = 72
-for alpha in [0,1,2,3]:
-    dp_sim = DiffractionSimulation(coordinates=dp_cord_list[alpha],
-                                   intensities=np.ones_like(dp_cord_list[alpha][:,0]),
-                                   )
-    dps.append(dp_sim.as_signal(2*half_side_length,0.025,max_r).data) #stores a numpy array of pattern
-# And onwards
-dpx = pxm.ElectronDiffraction(np.array([dps[0:2],dps[2:]])) #now from a 2x2 array of patterns
-peaks = hs.signals.Signal2D(np.array([dp_cord_list[0:2],dp_cord_list[2:]]))
-
-### And plot!
-
-mmx,mmy = generate_marker_inputs_from_peaks(peaks)
-dpx.set_calibration(2*max_r/(2*half_side_length))
-dpx.plot(cmap='viridis')
-for mx,my in zip(mmx,mmy):
-    m = hs.markers.point(x=mx,y=my,color='red',marker='x')
-    dpx.add_marker(m,plot_marker=True,permanent=True)
-
 def test_marker_placement_correct_beta():
+    dps = []
+    dp_cord_list= np.divide(generate_dp_cord_list(),80)
+    max_r = np.max(dp_cord_list) + 0.1
+    for coords in dp_cord_list:
+        dp_sim = DiffractionSimulation(coordinates=coords,
+                                       intensities=np.ones_like(coords[:,0]))
+        dps.append(dp_sim.as_signal(144,0.025,max_r).data) #stores a numpy array of pattern
+    dp = pxm.ElectronDiffraction(np.array([dps[0:2],dps[2:]])) #now from a 2x2 array of patterns
+    dp.set_calibration(2*max_r/(144))
+    local_plotter(dp,dp_cord_list)
+    
     #This is human assessed, if you see this comment, you should check it
     assert True
