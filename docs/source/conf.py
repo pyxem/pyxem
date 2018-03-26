d8e07875f6c35ea292055885f0d3f355e3928453
pc494 <pc494@cam.ac.uk>
2018-03-26 16:42:36 +0100

    reverting to mathjax

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
+    'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
