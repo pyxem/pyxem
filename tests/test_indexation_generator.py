897a3832669079ddafe0552874f7bfe4acb45bb1
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-03-28 15:23:22 +0100

    tidying up for updates

--- tests/test_indexation_generator.py
+++ tests/test_indexation_generator.py
@@ -15,3 +15,30 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+import pytest
+from pyxem.generators.indexation_generator import IndexationGenerator
+from pyxem import ElectronDiffraction
+
+
+@pytest.fixture
+def coords_intensity_simulation():
+    return DiffractionSimulation(coordinates = np.asarray([[0.3,1.2,0]]), intensities = np.ones(1))
+
+@pytest.fixture
+def get_signal():
+    size  = 144
+    sigma = 0.03
+    max_r = 1.5
+    return coords_intensity_simulation().as_signal(size,sigma,max_r)
+
+def test_typing():
+    assert type(get_signal()) is ElectronDiffraction
+
+def test_correct_quadrant_np():
+    A = get_signal().data
+    assert (np.sum(A[:72,:72]) == 0)
+    assert (np.sum(A[72:,:72]) == 0)
+    assert (np.sum(A[:72,72:]) == 0)
+    assert (np.sum(A[72:,72:])  > 0)
