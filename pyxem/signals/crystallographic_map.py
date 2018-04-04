f7fbc63eb91a83fa623b8c4c2dbee7a9f0ec9bb9
pc494 <pc494@cam.ac.uk>
2018-04-04 17:45:52 +0200

    clean up .save_map and intro .load_map

--- pyxem/signals/crystallographic_map.py
+++ pyxem/signals/crystallographic_map.py
@@ -101,15 +101,17 @@ class CrystallographicMap(BaseSignal):
         6 = x co-ord in navigation space, 
         7 = y co-ord in navigation space.
         """
-        results_array = np.zeros([0,7])
+        results_array = np.zeros([0,7]) #header row
         for i in range (0, self.data.shape[1]):
             for j in range (0, self.data.shape[0]):
-                # XXX
-                # This won't work for a multiphase sample, can't guarentee [0] is the best fit
-                try:
-                    newrow = self.inav[i,j].data[0,0:5]
-                except IndexError: #only 1 row at any given data point
-                    newrow = self.inav[i,j].data[0:5]
+                newrow = self.inav[i,j].data[0:5]
                 newrow = np.append(newrow, [i,j])
                 results_array = np.vstack([results_array, newrow])
         np.savetxt('{filename}', results_array, delimiter = "\t", newline="\r\n")
+
+    def load_map(filename):
+        """
+        Loads a map saved by previously saved via .save_map()
+        
+        """
+        raise NotImplementedError("Not yet needed")
