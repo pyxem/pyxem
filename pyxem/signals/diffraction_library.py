a80e079c98b8cfb663c88901b9b327668ddf43ed
pc494 <32455330+pc494@users.noreply.github.com>
2018-04-09 16:50:34 +0200

    Adding io for libraries (#163)

--- pyxem/signals/diffraction_library.py
+++ pyxem/signals/diffraction_library.py
@@ -16,6 +16,18 @@
 # You should have received a copy of the GNU General Public License
 # along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
 
+import pyxem as pxm
+import pickle
+
+def load_DiffractionLibrary(filename,safety=False):
+    if safety:
+        with open(filename, 'rb') as handle:
+            return pickle.load(handle)
+    else:
+        raise RuntimeError('Unpickling is risky, turn safety to True if \
+        trust the author of this content')
+
+
 class DiffractionLibrary(dict):
     """Maps crystal structure (phase) and orientation (Euler angles or
     axis-angle pair) to simulated diffraction data.
@@ -25,18 +37,18 @@ class DiffractionLibrary(dict):
         """ Extracts a single pattern for viewing,
         unspecified layers of dict are selected randomly and so this method
         is not entirely repeatable
-        
-        
+
+
         Parameters
         ----------
-        
+
         Phase : Label for the Phase you are interested in. Randomly chosen
         if False
-        Angle : Label for the Angle you are interested in. Randomly chosen 
+        Angle : Label for the Angle you are interested in. Randomly chosen
         if False
-        
+
         """
-        
+
         if phase:
             if angle:
                 return self[phase][angle]['Sim']
@@ -50,15 +62,6 @@ class DiffractionLibrary(dict):
                 for diffraction_pattern in diff_lib.values():
                     return diffraction_pattern['Sim']
 
-    def plot(self):
-        """Plots the library interactively. This is a helper method and it is
-        not tested.
-        """
-        from pyxem.signals.electron_diffraction import ElectronDiffraction
-        sim_diff_dat = []
-        for key in self.keys():
-            for ori in self[key].keys():
-                dpi = self[key][ori]['Sim'].as_signal(128, 0.03, 1)
-                sim_diff_dat.append(dpi.data)
-        ppt_test = ElectronDiffraction(sim_diff_dat)
-        ppt_test.plot()
+    def pickle_library(self,filename):
+        with open(filename, 'wb') as handle:
+            pickle.dump(self, handle, protocol=pickle.HIGHEST_PROTOCOL)
