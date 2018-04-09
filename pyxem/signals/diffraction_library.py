eaa6f18074035031f7c3eff92184238c92050797
pc494 <pc494@cam.ac.uk>
2018-04-09 15:20:16 +0200

    removing plot method

--- pyxem/signals/diffraction_library.py
+++ pyxem/signals/diffraction_library.py
@@ -62,19 +62,6 @@ class DiffractionLibrary(dict):
                 for diffraction_pattern in diff_lib.values():
                     return diffraction_pattern['Sim']
 
-    def plot(self):
-        """Plots the library interactively. This is a helper method and it is
-        not tested.
-        """
-        from pyxem.signals.electron_diffraction import ElectronDiffraction
-        sim_diff_dat = []
-        for key in self.keys():
-            for ori in self[key].keys():
-                dpi = self[key][ori]['Sim'].as_signal(128, 0.03, 1)
-                sim_diff_dat.append(dpi.data)
-        ppt_test = ElectronDiffraction(sim_diff_dat)
-        ppt_test.plot()
-
     def pickle_library(self,filename):
         with open(filename, 'wb') as handle:
             pickle.dump(self, handle, protocol=pickle.HIGHEST_PROTOCOL)
