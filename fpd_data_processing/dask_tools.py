46c451d168102322c171ba30cb954d175e4b0c89
Magnus Nord <magnunor@gmail.com>
2018-06-01 20:56:19 +0200

    Add functions for mask, threshold and center of mass using dask arrays

---
+++ fpd_data_processing/dask_tools.py
@@ -0,0 +1,181 @@
+import numpy as np
+import dask.array as da
+
+
+def _mask_array(dask_array, mask_array):
+    """Mask two last dimensions in a dask array.
+
+    Parameters
+    ----------
+    dask_array : Dask array
+    mask_array : NumPy array
+        True values will be masked.
+    mask_array : NumPy array
+        Array with bool values. The True values will be masked
+        (i.e. ignored). Must have the same shape as the two
+        last dimensions in dask_array.
+
+    Returns
+    -------
+    dask_array_masked : masked Dask array
+
+    Examples
+    --------
+    >>> import dask.array as da
+    >>> import fpd_data_processing.dask_tools as dt
+    >>> data = da.random.random(
+    ...     size=(32, 32, 128, 128), chunks=(16, 16, 128, 128))
+    >>> mask_array = np.ones(shape=(128, 128), dtype=bool)
+    >>> mask_array[64-10:64+10, 64-10:64+10] = False
+    >>> output_dask = dt._mask_array(data, mask_array=mask_array)
+    >>> output = output_dask.compute()
+
+    """
+    if not dask_array.shape[-2:] == mask_array.shape:
+        raise ValueError(
+                "mask_array ({0}) and last two dimensions in the "
+                "dask_array ({1}) need to have the same shape.".format(
+                    mask_array.shape, dask_array.shape[-2:]))
+    mask_array_4d = da.ones_like(dask_array, dtype=np.bool)
+    mask_array_4d = mask_array_4d[:, :] * mask_array
+    dask_array_masked = da.ma.masked_array(dask_array, mask_array_4d)
+    return dask_array_masked
+
+
+def _threshold_array(dask_array, threshold_value=1, mask_array=None):
+    """
+    Parameters
+    ----------
+    dask_array : Dask array
+        Must have either 2, 3 or 4 dimensions.
+    threshold_value : scalar, optional
+        Default value is 1.
+    mask_array : NumPy array, optional
+        Array with bool values. The True values will be masked
+        (i.e. ignored). Must have the same shape as the two
+        last dimensions in dask_array.
+
+    Returns
+    -------
+    thresholded_array : Dask array
+
+    Examples
+    --------
+    >>> import dask.array as da
+    >>> import fpd_data_processing.dask_tools as dt
+    >>> data = da.random.random(
+    ...     size=(32, 32, 128, 128), chunks=(16, 16, 128, 128))
+    >>> output_dask = dt._threshold_array(data)
+    >>> output = output_dask.compute()
+
+    Non-default threshold value
+
+    >>> output_dask = dt._threshold_array(data, threshold_value=1.5)
+    >>> output = output_dask.compute()
+
+    Masking everything except the center of the image
+
+    >>> mask_array = np.ones(shape=(128, 128), dtype=bool)
+    >>> mask_array[64-10:64+10, 64-10:64+10] = False
+    >>> output_dask = dt._threshold_array(data, mask_array=mask_array)
+    >>> output = output_dask.compute()
+
+    """
+    input_array = dask_array.copy()
+    if mask_array is not None:
+        input_array = _mask_array(input_array, mask_array)
+        dask_array = dask_array * np.invert(mask_array)
+    mean_array = da.mean(input_array, axis=(-2, -1))
+    threshold_array = mean_array * threshold_value
+
+    # Not very elegant solution, but works for the most common data dimensions
+    if len(dask_array.shape) == 4:
+        swaped_array = dask_array.swapaxes(0, 2).swapaxes(1, 3)
+        thresholded_array = swaped_array > threshold_array
+        thresholded_array = thresholded_array.swapaxes(1, 3).swapaxes(0, 2)
+    elif len(dask_array.shape) == 3:
+        swaped_array = dask_array.swapaxes(0, 1).swapaxes(1, 2)
+        thresholded_array = swaped_array > threshold_array
+        thresholded_array = thresholded_array.swapaxes(1, 2).swapaxes(0, 1)
+    elif len(dask_array.shape) == 2:
+        thresholded_array = dask_array > threshold_array
+    else:
+        raise ValueError(
+                "dask_array need to have either 2, 3, or 4 dimensions. "
+                "The input has {0} dimensions".format(len(dask_array.shape)))
+    thresholded_array = da.ma.getdata(thresholded_array)
+    return thresholded_array
+
+
+def _center_of_mass_array(dask_array, threshold_value=None, mask_array=None):
+    """Find center of mass of last two dimensions for a dask array.
+
+    The center of mass can be calculated using a mask and threshold.
+
+    Parameters
+    ----------
+    dask_array : Dask array
+        Must have either 2, 3 or 4 dimensions.
+    threshold_value : scalar, optional
+    mask_array : NumPy array, optional
+        Array with bool values. The True values will be masked
+        (i.e. ignored). Must have the same shape as the two
+        last dimensions in dask_array.
+
+    Returns
+    -------
+    center_of_mask_dask_array : Dask array
+
+    Examples
+    --------
+    >>> import dask.array as da
+    >>> import fpd_data_processing.dask_tools as dt
+    >>> data = da.random.random(
+    ...     size=(64, 64, 128, 128), chunks=(16, 16, 128, 128))
+    >>> output_dask = dt._center_of_mass_array(data)
+    >>> output = output_dask.compute()
+
+    Masking everything except the center of the image
+
+    >>> mask_array = np.ones(shape=(128, 128), dtype=bool)
+    >>> mask_array[64-10:64+10, 64-10:64+10] = False
+    >>> output_dask = dt._center_of_mass_array(data, mask_array=mask_array)
+    >>> output = output_dask.compute()
+
+    Masking and thresholding
+
+    >>> output_dask = dt._center_of_mass_array(
+    ...     data, mask_array=mask_array, threshold_value=3)
+    >>> output = output_dask.compute()
+
+    """
+    det_shape = dask_array.shape[-2:]
+    y_grad, x_grad = np.mgrid[0:det_shape[0], 0:det_shape[1]]
+    y_grad, x_grad = y_grad.astype(np.float64), x_grad.astype(np.float64)
+    sum_array = np.ones_like(x_grad)
+
+    if mask_array is not None:
+        if not mask_array.shape == det_shape:
+            raise ValueError(
+                    "mask_array ({0}) must have same shape as last two "
+                    "dimensions of the dask_array ({1})".format(
+                        mask_array.shape, det_shape))
+        x_grad = x_grad * np.invert(mask_array)
+        y_grad = y_grad * np.invert(mask_array)
+        sum_array = sum_array * np.invert(mask_array)
+    if threshold_value is not None:
+        dask_array = _threshold_array(
+                dask_array, threshold_value=threshold_value,
+                mask_array=mask_array)
+
+    x_shift = da.multiply(dask_array, x_grad, dtype=np.float64)
+    y_shift = da.multiply(dask_array, y_grad, dtype=np.float64)
+    sum_array = da.multiply(dask_array, sum_array, dtype=np.float64)
+
+    x_shift = np.sum(x_shift, axis=(-2, -1), dtype=np.float64)
+    y_shift = np.sum(y_shift, axis=(-2, -1), dtype=np.float64)
+    sum_array = np.sum(sum_array, axis=(-2, -1), dtype=np.float64)
+
+    beam_shifts = da.stack((x_shift, y_shift))
+    beam_shifts = da.divide(beam_shifts[:], sum_array, dtype=np.float64)
+    return beam_shifts
