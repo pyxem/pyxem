915509c09831e08cdc5cfceea97202041b8ff380
Magnus Nord <magnunor@gmail.com>
2018-07-26 18:04:38 +0200

    Change rest of unittest to pytest

--- pixstem/tests/test_radial.py
+++ pixstem/tests/test_radial.py
@@ -1,33 +1,34 @@
-import unittest
+import pytest
+from pytest import approx
 import numpy as np
 import pixstem.api as ps
 import pixstem.radial as ra
 import pixstem.make_diffraction_test_data as mdtd
 
 
-class test_radial_module(unittest.TestCase):
+class TestRadialModule:
 
     def test_centre_comparison(self):
         s = ps.PixelatedSTEM(np.ones((20, 20)))
         s_list = ra._centre_comparison(s, 1, 1)
-        self.assertEqual(len(s_list), 9)
+        assert len(s_list) == 9
 
         s1 = ps.PixelatedSTEM(np.ones((5, 20, 20)))
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ra._centre_comparison(s1, 1, 1)
 
         s2 = ps.PixelatedSTEM(np.ones((30, 30)))
         s2_list = ra._centre_comparison(s2, 1, 1, angleN=20)
         for temp_s in s2_list:
-            self.assertEqual(temp_s.axes_manager.navigation_shape, (20, ))
+            assert temp_s.axes_manager.navigation_shape == (20, )
 
         s3 = ps.PixelatedSTEM(np.ones((40, 40)))
         s3_list = ra._centre_comparison(
                 s3, 1, 1, angleN=10,
                 crop_radial_signal=(3, 8))
         for temp_s in s3_list:
-            self.assertEqual(temp_s.axes_manager.signal_shape, (5, ))
-            self.assertEqual(temp_s.axes_manager.navigation_shape, (10, ))
+            assert temp_s.axes_manager.signal_shape == (5, )
+            assert temp_s.axes_manager.navigation_shape == (10, )
 
     def test_get_coordinate_of_min(self):
         array = np.ones((10, 10))*10
@@ -41,7 +42,7 @@ class test_radial_module(unittest.TestCase):
         min_pos = ra.get_coordinate_of_min(s)
         # min_pos[0] (x) should be at (7*0.5) + 55 = 58.5
         # min_pos[1] (y) should be at (5*0.4) + 50 = 52.
-        self.assertEqual(min_pos, (58.5, 52.0))
+        assert min_pos == (58.5, 52.0)
 
     def test_get_optimal_centre_position(self):
         x0, y0 = 300., 300.
@@ -53,8 +54,8 @@ class test_radial_module(unittest.TestCase):
         s_centre_position = ra.get_optimal_centre_position(
                 s, radial_signal_span=(180, 210), steps=2, step_size=1)
         x, y = ps.radial.get_coordinate_of_min(s_centre_position)
-        self.assertTrue((x0 - 0.5) <= x and x <= (x0 + 0.5))
-        self.assertTrue((x0 - 0.5) <= x and x <= (x0 + 0.5))
+        assert (x0 - 0.5) <= x and x <= (x0 + 0.5)
+        assert (x0 - 0.5) <= x and x <= (x0 + 0.5)
 
     def test_get_optimal_centre_position_off_centre_non_square(self):
         test_data = mdtd.MakeTestData(
@@ -89,8 +90,8 @@ class test_radial_module(unittest.TestCase):
         min_pos3 = ra.get_coordinate_of_min(s_c)
 
         for min_pos in [min_pos0, min_pos1, min_pos2, min_pos3]:
-            self.assertAlmostEqual(min_pos[0], x0)
-            self.assertAlmostEqual(min_pos[1], y0)
+            assert approx(min_pos[0]) == x0
+            assert approx(min_pos[1]) == y0
 
     def test_get_radius_vs_angle(self):
         test_data = mdtd.MakeTestData(
@@ -117,13 +118,13 @@ class test_radial_module(unittest.TestCase):
         s.axes_manager.signal_axes[0].offset = -x+1
         s.axes_manager.signal_axes[1].offset = -y-1
         ra.refine_signal_centre_position(s, (10., 20.), angleN=6)
-        self.assertEqual(s.axes_manager.signal_axes[0].offset, -x)
-        self.assertEqual(s.axes_manager.signal_axes[1].offset, -y)
+        assert s.axes_manager.signal_axes[0].offset == -x
+        assert s.axes_manager.signal_axes[1].offset == -y
 
 
-class test_get_angle_image_comparison(unittest.TestCase):
+class TestGetAngleImageComparison:
 
-    def setUp(self):
+    def setup_method(self):
         self.r0, self.r1 = 10, 20
         test_data0 = mdtd.MakeTestData(100, 100)
         test_data0.add_ring(50, 50, self.r0)
@@ -142,32 +143,32 @@ class test_get_angle_image_comparison(unittest.TestCase):
     def test_correct_radius(self):
         s0, s1, r0, r1 = self.s0, self.s1, self.r0, self.r1
         s = ra.get_angle_image_comparison(s0, s1, angleN=2)
-        self.assertEqual(s.axes_manager.signal_shape, (100, 100))
+        assert s.axes_manager.signal_shape == (100, 100)
         # Check that radius is correct by getting a line profile
         s_top = s.isig[0., :0.]
         s_bot = s.isig[0., 0.:]
         argmax0 = s_top.data.argmax()
         argmax1 = s_bot.data.argmax()
-        self.assertEqual(abs(s_top.axes_manager[0].index2value(argmax0)), r0)
-        self.assertEqual(abs(s_bot.axes_manager[0].index2value(argmax1)), r1)
+        assert abs(s_top.axes_manager[0].index2value(argmax0)) == r0
+        assert abs(s_bot.axes_manager[0].index2value(argmax1)) == r1
 
     def test_different_signal_size(self):
         s0 = mdtd.MakeTestData(100, 100).signal
         s1 = mdtd.MakeTestData(100, 150).signal
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ra.get_angle_image_comparison(s0, s1)
 
     def test_mask(self):
         s0, s1 = self.s0, self.s1
         s_no_mask = ra.get_angle_image_comparison(s0, s1)
         s_mask = ra.get_angle_image_comparison(s0, s1, mask_radius=40)
-        self.assertNotEqual(s_no_mask.data.sum(), 0.0)
-        self.assertEqual(s_mask.data.sum(), 0.0)
+        assert s_no_mask.data.sum() != 0.0
+        assert s_mask.data.sum() == 0.0
 
 
-class test_fit_ellipse(unittest.TestCase):
+class TestFitEllipse:
 
-    def setUp(self):
+    def setup_method(self):
         axis1, axis2 = 40, 70
         s = ps.PixelatedSTEM(np.zeros((200, 220)))
         s.axes_manager[0].offset, s.axes_manager[1].offset = -100, -110
@@ -186,10 +187,10 @@ class test_fit_ellipse(unittest.TestCase):
         ellipse_parameters = ra._fit_ellipse_to_xy_points(x, y)
         xC, yC, semi_len0, semi_len1, rot, eccen = ra._get_ellipse_parameters(
                 ellipse_parameters)
-        self.assertAlmostEqual(xC, 0., places=1)
-        self.assertAlmostEqual(yC, 0., places=1)
-        self.assertAlmostEqual(semi_len0, axis2, places=-1)
-        self.assertAlmostEqual(semi_len1, axis1, places=-1)
+        assert approx(xC, abs=0.1) == 0.
+        assert approx(yC, abs=0.1) == 0.
+        assert approx(semi_len0, abs=1.) == axis2
+        assert approx(semi_len1, abs=1.) == axis1
 
     def test_get_signal_with_markers(self):
         s = self.s
@@ -208,11 +209,11 @@ class test_fit_ellipse(unittest.TestCase):
         output = ra.fit_single_ellipse_to_signal(
                 s, (50, 70), angleN=10, show_progressbar=False)
         output[0].plot()
-        self.assertAlmostEqual(output[1], 0., places=2)
-        self.assertAlmostEqual(output[2], 0., places=2)
-        self.assertAlmostEqual(output[3], 60, places=-1)
-        self.assertAlmostEqual(output[4], 60, places=-1)
-        self.assertAlmostEqual(output[6], 1., places=5)
+        assert approx(output[1], abs=0.01) == 0.
+        assert approx(output[2], abs=0.01) == 0.
+        assert approx(output[3], abs=0.6) == 60
+        assert approx(output[4], abs=0.6) == 60
+        assert approx(output[6], abs=0.000001) == 1.
 
     def test_fit_single_ellipse_to_signal_rotation(self):
         rot_list = [
@@ -229,7 +230,7 @@ class test_fit_ellipse(unittest.TestCase):
             output = ra.fit_single_ellipse_to_signal(
                     s, (50, 80), angleN=10, show_progressbar=False)
             output_rot = output[5] % np.pi
-            self.assertAlmostEqual(output_rot, rot % np.pi, places=1)
+            assert approx(output_rot, abs=0.1) == (rot % np.pi)
         for rot in rot_list:
             s = ps.PixelatedSTEM(np.zeros((200, 200)))
             s.axes_manager[0].offset, s.axes_manager[1].offset = -100, -100
@@ -240,7 +241,7 @@ class test_fit_ellipse(unittest.TestCase):
             output = ra.fit_single_ellipse_to_signal(
                     s, (50, 80), angleN=10, show_progressbar=False)
             output_rot = (output[5] + np.pi/2) % np.pi
-            self.assertAlmostEqual(output_rot, rot % np.pi, places=1)
+            assert approx(output_rot, abs=0.1) == (rot % np.pi)
 
     def test_fit_ellipses_to_signal(self):
         s = ps.PixelatedSTEM(np.zeros((200, 220)))
@@ -257,23 +258,23 @@ class test_fit_ellipse(unittest.TestCase):
                 s, [(50, 70), (70, 95)], angleN=[20, 30],
                 show_progressbar=False)
         output1[0].plot()
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ra.fit_ellipses_to_signal(
                     s, [(50, 70), (70, 95), (80, 105)],
                     angleN=[20, 30], show_progressbar=False)
 
 
-class test_holz_calibration(unittest.TestCase):
+class TestHolzCalibration:
 
     def test_get_holz_angle(self):
         wavelength = 2.51/1000
         lattice_parameter = 0.3905*2**0.5
         angle = ra._get_holz_angle(wavelength, lattice_parameter)
-        self.assertAlmostEqual(95.37805/1000, angle, places=4)
+        assert approx(95.37805/1000) == angle
 
     def test_scattering_angle_to_lattice_parameter(self):
         wavelength = 2.51/1000
         angle = 95.37805/1000
         lattice_size = ra._scattering_angle_to_lattice_parameter(
                 wavelength, angle)
-        self.assertAlmostEqual(0.55225047, lattice_size, places=4)
+        assert approx(0.55225047) == lattice_size
