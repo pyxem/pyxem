f808ad6a05a7e49081bb0c5ced4eb8246284639e
Phillip <pc494@cam.ac.uk>
2018-07-27 14:34:52 +0100

    affine_transform() comments added

--- pyxem/utils/expt_utils.py
+++ pyxem/utils/expt_utils.py
@@ -282,8 +282,12 @@ def affine_transformation(z,matrix,order,**kwargs):
     shift_y, shift_x = np.array(z.shape[:2]) / 2.
     tf_shift = tf.SimilarityTransform(translation=[-shift_x, -shift_y])
     tf_shift_inv = tf.SimilarityTransform(translation=[shift_x, shift_y])
-
+    
+    # This defines the transform you want to perform
     transformation = tf.AffineTransform(matrix=matrix)
+    
+    #skimage transforms can be added like this, actually matrix multiplication,
+    #hence the need for the brackets. (Note tf.warp takes the inverse)
     trans = tf.warp(z, (tf_shift + (transformation + tf_shift_inv)).inverse,
                     order=order,**kwargs)
 
