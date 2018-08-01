59a41884ac61f3ee5c29e85c116a4d584c921aa7
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-08-01 16:30:06 +0100

    remove trailing comma

--- tests/test_utils/test_expt_utils.py
+++ tests/test_utils/test_expt_utils.py
@@ -18,7 +18,7 @@
 
 import pytest
 import numpy as np
-from pyxem.utils.expt_utils import _cart2polar, _polar2cart,
+from pyxem.utils.expt_utils import _cart2polar, _polar2cart
 from pyxem.utils.expt_utils import *
 
 @pytest.mark.parametrize('x, y, r, theta',[
