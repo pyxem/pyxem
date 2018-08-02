a98d873742a0ae24ab3fedcf294504cd03948df9
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-08-02 17:01:44 +0100

    test ProfileIndexationGenerator init

--- tests/test_generators/test_indexation_generator.py
+++ tests/test_generators/test_indexation_generator.py
@@ -59,3 +59,19 @@ def profile_simulation():
                                      {(6, 2, 0): 24},
                                      {(5, 3, 3): 24},
                                      {(4, 4, 4): 8}])
+
+def test_profile_indexation_generator_init(profile_simulation):
+    pig = ProfileIndexationGenerator(magnitudes=[0.31891931643691351,
+                                                 0.52079306292509475,
+                                                 0.6106839974876449,
+                                                 0.73651261277849378,
+                                                 0.80259601243613932,
+                                                 0.9020400452156796,
+                                                 0.95675794931074043,
+                                                 1.0415861258501895,
+                                                 1.0893168446141808,
+                                                 1.1645286909108374,
+                                                 1.2074090451670043,
+                                                 1.2756772657476541],
+                                    simulation=profile_simulation)
+    assert isinstance(pig, ProfileIndexationGenerator)
