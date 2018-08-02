bd388435947425a9cba74241065dd3709dcffa0e
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-08-02 16:40:13 +0100

    add _ for unused variable in calculate_profile_data

--- tests/test_signals/test_diffraction_simulation.py
+++ tests/test_signals/test_diffraction_simulation.py
@@ -19,6 +19,7 @@
 import numpy as np
 import pytest
 from pyxem.signals.diffraction_simulation import DiffractionSimulation
+from pyxem.signals.diffraction_simulation import ProfileSimulation
 from pyxem.generators.diffraction_generator import DiffractionGenerator
 from pyxem import ElectronDiffraction
 import pymatgen as pmg
