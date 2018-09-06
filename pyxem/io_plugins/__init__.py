cbbc152d22958b681838e6300ec6e6820f2bc01b
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-09-06 09:46:32 +0100

    update io_plugins import for hyperspy 1.4

--- pyxem/io_plugins/__init__.py
+++ pyxem/io_plugins/__init__.py
@@ -21,12 +21,12 @@ import logging
 
 from hyperspy.io_plugins import (msa, digital_micrograph, fei, mrc, ripple,
                                  tiff, semper_unf, blockfile, dens, emd,
-                                 protochips, edax, bcf)
+                                 protochips, edax, bruker)
 
 from pyxem.io_plugins import mib
 
 io_plugins = [msa, digital_micrograph, fei, mrc, ripple, tiff, semper_unf,
-              blockfile, dens, emd, protochips, edax, bcf, mib]
+              blockfile, dens, emd, protochips, edax, bruker, mib]
 
 _logger = logging.getLogger(__name__)
 
