e3eaea7ecece200fb2d48bbb470ecae3fb481eb0
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-09-06 23:43:09 +0100

    DiffractionVarianceProfile implementation

--- pyxem/signals/diffraction_variance.py
+++ pyxem/signals/diffraction_variance.py
@@ -21,12 +21,41 @@
 
 from hyperspy.signals import Signal2D
 
+from pyxem.signals.variance_profile import DiffractionVarianceProfile
+
+from pyxem.utils.expt_utils import radial_average
+
 class DiffractionVariance(Signal2D):
     _signal_type = "diffraction_variance"
 
     def __init__(self, *args, **kwargs):
         Signal2D.__init__(self, *args, **kwargs)
 
+    def get_radial_profile(self,inplace=False,**kwargs):
+        """Return the radial profile of the diffraction variance signals.
+
+        Returns
+        -------
+        radial_profile: :obj:`hyperspy.signals.Signal1D`
+            The radial average profile of each diffraction pattern
+            in the ElectronDiffraction signal as a Signal1D.
+
+        See also
+        --------
+        :func:`pyxem.utils.expt_utils.radial_average`
+
+        Examples
+        --------
+        .. code-block:: python
+            profiles = ed.get_radial_profile()
+            profiles.plot()
+        """
+        radial_profiles = self.map(radial_average,
+                                   inplace=inplace,**kwargs)
+
+        radial_profiles.axes_manager.signal_axes[0].offset = 0
+        signal_axis = radial_profiles.axes_manager.signal_axes[0]
+        return DiffractionVarianceProfile(radial_profiles.as_signal1D(signal_axis))
 
 class ImageVariance(Signal2D):
     _signal_type = "image_variance"
