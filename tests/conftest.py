6aa1d58e6beb93387dc0befb0425258e76984322
pc494 <pc494@cam.ac.uk>
2018-09-07 16:50:03 +0100

    adding conftest

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+import diffpy.structure
+
+@pytest.fixture(scope="session",autouse=True)
+def default_structure():
+    latt = diffpy.structure.lattice.Lattice(3,3,5,90,90,120)
+    atom = diffpy.structure.atom.Atom(atype='Ni',xyz=[0,0,0],lattice=latt)
+    hexagonal_structure = diffpy.structure.Structure(atoms=[atom],lattice=latt)
+    return hexagonal_structure
