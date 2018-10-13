94c5cdc849631aaefc09a7b2e825bc3216a37e29
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-10-13 10:28:03 +0100

    VectorLibary initial outline

---
+++ pyxem/libraries/vector_library.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2018 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+import pickle
+import numpy as np
+
+def load_VectorLibrary(filename,safety=False):
+    if safety:
+        with open(filename, 'rb') as handle:
+            return pickle.load(handle)
+    else:
+        raise RuntimeError('Unpickling is risky, turn safety to True if \
+        trust the author of this content')
+
+
+class VectorLibrary(dict):
+    """Maps crystal structure (phase) and orientation (Euler angles or
+    axis-angle pair) to simulated diffraction data.
+    """
+
+    def pickle_library(self,filename):
+        with open(filename, 'wb') as handle:
+            pickle.dump(self, handle, protocol=pickle.HIGHEST_PROTOCOL)
