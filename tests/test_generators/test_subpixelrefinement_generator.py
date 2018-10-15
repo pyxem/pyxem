22647e4703814fd5f12b25c28957af4e433eb91c
Phillip <pc494@cam.ac.uk>
2018-10-15 18:26:48 +0100

    introduce arguments to the testing suite

--- tests/test_generators/test_subpixelrefinement_generator.py
+++ tests/test_generators/test_subpixelrefinement_generator.py
@@ -17,16 +17,18 @@
 # along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
 
 import pytest
+import numpy as np
+
 from pyxem.generators.subpixelrefinement_generator import SubpixelrefinementGenerator
 
 @pytest.fixture
-def SPR_generator(diffraction_pattern,vect = [[1,1]]):
+def SPR_generator(diffraction_pattern,vect = np.asarray([[4,4]])):
     return SubpixelrefinementGenerator(diffraction_pattern,vect)
 
 def test_conventional_xc(SPR_generator):
-    diff_vect = SPR_generator.conventional_xc()
+    diff_vect = SPR_generator.conventional_xc(4,2,100)
     assert True
 
 def test_sobel_filtered_xc(SPR_generator):
-    diff_vect = SPR_generator.sobel_filtered_xc()
+    diff_vect = SPR_generator.sobel_filtered_xc(4,2,100)
     assert True
