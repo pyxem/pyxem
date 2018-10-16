ee5b1775c186755f0e213a2f99f0fe6a5344baa2
Phillip <pc494@cam.ac.uk>
2018-10-16 14:32:21 +0100

    bulking out to include assertions in subpixelrefinement tests

--- tests/test_generators/test_subpixelrefinement_generator.py
+++ tests/test_generators/test_subpixelrefinement_generator.py
@@ -19,7 +19,10 @@
 import pytest
 import numpy as np
 
+import pyxem as pxm
 from pyxem.generators.subpixelrefinement_generator import SubpixelrefinementGenerator
+from skimage import draw
+
 
 @pytest.fixture
 def SPR_generator(diffraction_pattern,vect = np.asarray([[4,4]])):
@@ -32,3 +35,21 @@ def test_conventional_xc(SPR_generator):
 def test_sobel_filtered_xc(SPR_generator):
     diff_vect = SPR_generator.sobel_filtered_xc(4,2,100)
     assert True
+
+@pytest.fixture()
+def create_spot():
+    z = np.zeros((128,128))
+    for r in [4,3,2]:
+        rr, cc = draw.circle(90,30,radius=r,shape=z.shape)
+        z[rr, cc] = 1 / r
+    dp = pxm.ElectronDiffraction(np.asarray([[z,z],[z,z]])) #this needs to be in 2x2
+    return dp
+
+
+def test_assertioned_xc(create_spot):
+    spr = SubpixelrefinementGenerator(create_spot,np.asarray([[90,30]]))
+    s = spr.conventional_xc(12,4,8)
+    error = np.subtract(spr.vectors_out[0,0],np.asarray([[90,30]]))
+    rms_error = np.sqrt(error[0,0]**2+error[0,1]**2)
+    assert rms_error < 0.2 #1/5th a pixel
+    assert rms_error == 1
