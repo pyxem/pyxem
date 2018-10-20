e77ee4ccb4ccc5c531bc999786582cbb43bc97fd
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-10-20 17:39:38 +0100

    plot_best_matching_results_on_signal() method

--- pyxem/signals/indexation_results.py
+++ pyxem/signals/indexation_results.py
@@ -17,7 +17,12 @@
 # along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
+import hyperspy.api as hs
 from hyperspy.signal import BaseSignal
+
+from pyxem.utils.sim_utils import peaks_from_best_template
+from pyxem.utils.plot import generate_marker_inputs_from_peaks
+
 
 from pyxem import CrystallographicMap
 
@@ -65,6 +70,30 @@ class IndexationResults(BaseSignal):
     def __init__(self, *args, **kwargs):
         BaseSignal.__init__(self, *args, **kwargs)
         self.axes_manager.set_signal_dimension(2)
+
+    def plot_best_matching_results_on_signal(self, signal,
+                                             phase, library,
+                                             *args, **kwargs):
+        """Plot the diffraction vectors on a signal.
+
+        Parameters
+        ----------
+        signal : ElectronDiffraction
+            The ElectronDiffraction signal object on which to plot the peaks.
+            This signal must have the same navigation dimensions as the peaks.
+        *args :
+            Arguments passed to signal.plot()
+        **kwargs :
+            Keyword arguments passed to signal.plot()
+        """
+        match_peaks = match_results.map(peaks_from_best_template,
+                                        phase=phase, library=library,
+                                        inplace=False)
+        mmx,mmy = generate_marker_inputs_from_peaks(match_peaks)
+        signal.plot(*args, **kwargs)
+        for mx,my in zip(mmx,mmy):
+            m = hs.markers.point(x=mx,y=my,color='red',marker='x')
+            signal.add_marker(m,plot_marker=True,permanent=True)
 
     def get_crystallographic_map(self,
                                  *args, **kwargs):
