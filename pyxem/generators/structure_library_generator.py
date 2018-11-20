4941159dee17ccfc63b9e4c6f7359bff5edcd9ff
Simon Høgås <simhog@hotmail.no>
2018-11-20 08:25:39 +0100

    Structure library generator docstrings

--- pyxem/generators/structure_library_generator.py
+++ pyxem/generators/structure_library_generator.py
@@ -33,17 +33,74 @@ stereographic_corners = {
 
 
 class StructureLibraryGenerator:
+    """Generates a structure library for the given phases
+
+    Parameters
+    ----------
+    phases : list
+        Array of three-component phase descriptions, where the phase
+        description is [<phase name> : string, <structure> :
+        diffpy.structure.Structure, <crystal system> : string], and crystal
+        system is one of 'cubic', 'hexagonal', 'orthorombic', 'tetragonal',
+        'trigonal', 'monoclinic'.
+
+    Attributes
+    ----------
+    phase_names : list of string
+        List of phase names.
+    structures : list of diffpy.structure.Structure
+        List of structures.
+    systems : list of string
+        List of crystal systems.
+
+    Examples
+    --------
+    >>> gen = StructureLibraryGenerator([
+    ...     ('ZB', structure_zb, 'cubic'),
+    ...     ('WZ', structure_wz, 'hexagonal')])
+    """
+
     def __init__(self, phases):
         self.phase_names = [phase[0] for phase in phases]
         self.structures = [phase[1] for phase in phases]
         self.systems = [phase[2] for phase in phases]
 
 
-    def get_orientations_from_list(self, rotation_lists):
-        return StructureLibrary(self.phase_names, self.structures, rotation_lists)
+    def get_orientations_from_list(self, orientations):
+        """Create a structure library from a list of rotations.
+
+        Parameters
+        ----------
+        orientations : list
+            A list over identifiers of lists of euler angles (as tuples) in the rzxz
+            convention and in degrees.
+
+        Returns
+        -------
+        structure_library : StructureLibrary
+            Structure library for the given phase names, structures and orientations.
+        """
+        return StructureLibrary(self.phase_names, self.structures, orientations)
 
 
     def get_orientations_from_stereographic_triangle(self, inplane_rotations, resolution):
+        """
+        Create a structure library from the stereographic triangles of the
+        given crystal systems.
+
+        Parameters
+        ----------
+        inplane_rotations : list
+            List over identifiers of lists of inplane rotations of the
+            diffraction patterns, in degrees.
+        resolution : float
+            Rotation list resolution in degrees.
+
+        Returns
+        -------
+        structure_library : StructureLibrary
+            Structure library for the given phase names, structures and crystal system.
+        """
         rotation_lists = [
                 rotation_list_stereographic(structure, *stereographic_corners[system], inplane_rotation, resolution)
                     for phase_name, structure, system, inplane_rotation in
