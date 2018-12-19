ba167a11bb6ac24ca2e4c541d5b049ce860966ac
Magnus Nord <magnunor@gmail.com>
2018-12-19 17:19:18 +0100

    Add marker_tools._get_4d_line_segment_list

--- pixstem/marker_tools.py
+++ pixstem/marker_tools.py
@@ -63,6 +63,100 @@ def _get_4d_points_marker_list(peaks_list, signal_axes=None, color='red',
     return marker_list
 
 
+def _get_4d_line_segment_list(lines_array, signal_axes=None, color='red',
+                              linewidth=1, linestyle='solid'):
+    """Get a list of 4 dimensional line segments markers.
+
+    The markers will be displayed on the signal dimensions.
+
+    Parameters
+    ----------
+    lines_array : 4D NumPy array
+    signal_axes : HyperSpy axes_manager object
+    color : string, optional
+        Color of point marker. Default 'red'.
+    linewidth : scalar, optional
+        Default 2
+    linestyle : string, optional
+        Default 'solid'
+
+    Returns
+    -------
+    marker_list : list of HyperSpy marker objects
+
+    """
+    max_lines = 0
+    for ix, iy in np.ndindex(lines_array.shape[:2]):
+        lines_list = lines_array[ix, iy]
+        if lines_list is not None:
+            n_lines = len(lines_list)
+            if n_lines > max_lines:
+                max_lines = n_lines
+
+    marker_array_shape = (lines_array.shape[0], lines_array.shape[1],
+                          max_lines)
+    marker_x1_array = np.ones(marker_array_shape) * -1000
+    marker_y1_array = np.ones(marker_array_shape) * -1000
+    marker_x2_array = np.ones(marker_array_shape) * -1000
+    marker_y2_array = np.ones(marker_array_shape) * -1000
+    for ix, iy in np.ndindex(marker_x1_array.shape[:2]):
+        lines_list = lines_array[ix, iy]
+        if lines_list is not None:
+            for i_p, line in enumerate(lines_list):
+                if signal_axes is None:
+                    marker_x1_array[ix, iy, i_p] = line[1]
+                    marker_y1_array[ix, iy, i_p] = line[0]
+                    marker_x2_array[ix, iy, i_p] = line[3]
+                    marker_y2_array[ix, iy, i_p] = line[2]
+                else:
+                    sa0iv = signal_axes[0].index2value
+                    sa1iv = signal_axes[1].index2value
+                    marker_x1_array[ix, iy, i_p] = sa0iv(int(line[1]))
+                    marker_y1_array[ix, iy, i_p] = sa1iv(int(line[0]))
+                    marker_x2_array[ix, iy, i_p] = sa0iv(int(line[3]))
+                    marker_y2_array[ix, iy, i_p] = sa1iv(int(line[2]))
+
+    marker_list = []
+    for i_p in range(max_lines):
+        marker = hm.line_segment(
+                marker_x1_array[..., i_p], marker_y1_array[..., i_p],
+                marker_x2_array[..., i_p], marker_y2_array[..., i_p],
+                color=color, linewidth=linewidth, linestyle=linestyle)
+        marker_list.append(marker)
+    return marker_list
+
+
+def _get_2d_line_segment_list(lines_list, signal_axes=None, color='red',
+                              linewidth=1, linestyle='solid'):
+    """Get a list of 2d dimensional line segments markers.
+
+    The markers will be displayed on the signal dimensions.
+
+    Parameters
+    ----------
+    lines_list : list
+        In form [[x01, y01, x02, y02], [x11, y11, x12, y12], ...]
+    signal_axes : HyperSpy axes_manager object
+    color : string, optional
+        Color of point marker. Default 'red'.
+    linewidth : scalar, optional
+        Default 2
+    linestyle : string, optional
+        Default 'solid'
+
+    Returns
+    -------
+    marker_list : list of HyperSpy marker objects
+
+    """
+    marker_list = []
+    for x1, y1, x2, y2 in lines_list:
+        marker = hm.line_segment(x1, y1, x2, y2, color=color,
+                                 linewidth=linewidth, linestyle=linestyle)
+        marker_list.append(marker)
+    return marker_list
+
+
 def _add_permanent_markers_to_signal(signal, marker_list):
     """Add a list of markers to a signal.
 
