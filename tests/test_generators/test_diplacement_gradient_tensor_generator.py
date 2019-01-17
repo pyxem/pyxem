d1fabc184cd6e1383310d29cfa30660b3d1827f7
pc494 <pc494@cam.ac.uk>
2019-01-17 17:07:31 +0000

    implementing testing for strain map finding

--- tests/test_generators/test_diplacement_gradient_tensor_generator.py
+++ tests/test_generators/test_diplacement_gradient_tensor_generator.py
@@ -18,7 +18,9 @@
 
 import numpy as np
 import pytest
-import hyperspy.api as
+import hyperspy.api as hs
+
+from pyxem.generators.displacement_gradient_tensor_generator import *
 
 def rotation(z):
      theta = np.deg2rad(3)
@@ -38,12 +40,15 @@ def generate_test_vectors(v):
                        [uniform_expansion(v),stretch_in_x(v)]])
 
 
-xy = np.asarray([[1,0],[0,1]])
-oo = np.asarray(([1,1],[1,-1]))
-
-# parameterise these into pytest lingo
-dp_xy = hs.signals.Signal2D(generate_test_vectors(xy))
-dp_oo = hs.signals.Signal2D(generate_test_vectors(oo))
-
-D_xy      =
-xy_strain =
+def test_strain_mapping():
+    # this could be rephrased in terms of fixtures
+    xy = np.asarray([[1,0],[0,2]])
+    oo = np.asarray(([1,2],[3,-4]))
+    dp_xy = hs.signals.Signal2D(generate_test_vectors(xy))
+    dp_oo = hs.signals.Signal2D(generate_test_vectors(oo))
+    s_xy = get_DisplacementGradientMap(dp_xy,[[1,0],[0,2]]).get_strain_maps()
+    s_oo = get_DisplacementGradientMap(dp_oo,[[1,2],[3,-4]]).get_strain_maps()
+    np.testing.assert_almost_equal(s_xy.data,s_oo.data)
+    ### ALERT to the minus sign we have had to drop in
+    np.testing.assert_almost_equal(np.sum(s_xy.inav[3].data),-1*np.deg2rad(3)) #only one rotations occurs so you can use sum
+    np.testing.assert_almost_equal(np.sum(s_oo.inav[3].data),-1*np.deg2rad(3))
