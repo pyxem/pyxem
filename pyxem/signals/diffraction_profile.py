ff28cc92a42c19f1befab6d762acd364abe08e83
Jordi <9082687+jordiferrero@users.noreply.github.com>
2019-02-19 17:53:31 +0000

    Extended DiffractionProfile adding calibrationAxes function
    1) Added the function detector_axes_to_1D_kspace():
    It converts the detector 1D coordinates in px, to the respective 1D coordinates in the kspace, in Angstom^-1, using purely geometrical arguments.
    NOTE: It assumes that the detector_2_sample distance has been obtained from a calibration file.

--- pyxem/signals/diffraction_profile.py
+++ pyxem/signals/diffraction_profile.py
@@ -30,6 +30,39 @@ class ElectronDiffractionProfile(Signal1D):
     def __init__(self, *args, **kwargs):
         Signal1D.__init__(self, *args, **kwargs)
 
+    def detector_axes_to_1D_kspace(self, beam_wavelen, det2sample_len, pixel_len):
+        """Converts the detector 1D coordinates in px, to the respective 1D coordinates in the kspace, in Angstom^-1, using purely geometrical arguments. 
+        Only use for DiffractionProfile class. 2D version of the detector_to_3D_kspace from the class DiffractionVectors.
+        Args:
+        -------
+            beam_wavelen (float):
+                Wavelength of the scanning beam, in Amstrong.
+            det2sample_len (float): 
+                Distance from detector to sample, in Amstrong. IMPORTANT: Exact distance obtained from the calibration file.
+            pixel_len (float):
+                Length of the pixel in the detector, in micrometres
+        Returns:
+        -------
+            self:
+                DiffractionProfile has been modified from px cordinates to angstrom^-1
+        """
+        #Extract the pixel-coordinates as an array
+        px = np.array(self.axes_manager['k'].axis)
+        #Convert each pixel to the actual disctance in Angstrom
+        x = px*10000*pixel_len
+        # Get the theta angle (in 1D Edwald circunference it is arctan(px/d)) for each x:
+        theta = np.arctan(x/(det2sample_len))
+        #Convert each x to the respective gx value:
+        gx = (1/beam_wavelen)*np.sin(theta)
+        #Convert each x to the respective gy value:
+        gy = (1/beam_wavelen)*(1-np.cos(theta))
+        #Get the diffraction vector g magnitude:
+        g = np.sqrt(gx**2 + gy**2)
+
+        # Replace pixel coordinates in the ElectronDiffractionProfile to the kx values:
+        self.axes_manager['k'].axis = g
+        self.axes_manager['k'].name = '$\mid g \mid$'
+
     def find_peaks_1D(self, *args, **kwargs):
         """Find peaks in a DiffractionProfile using the O-Haver function (Hyperspy). It returns the DiffractionPeaks class.
         Parameters
