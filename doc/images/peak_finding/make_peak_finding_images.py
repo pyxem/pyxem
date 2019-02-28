daaea4e9d489710aaa03b49a49be2d10233f0025
Magnus Nord <magnunor@gmail.com>
2019-02-28 10:17:29 +0100

    DOC: add s.add_peak_array_as_markers to the user guide

--- doc/images/peak_finding/make_peak_finding_images.py
+++ doc/images/peak_finding/make_peak_finding_images.py
@@ -1,12 +1,12 @@
 import matplotlib
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 import pixstem.api as ps
-import pixstem.marker_tools as mt
 
 # Finding peaks
 s = ps.dummy_data.get_cbed_signal()
 peak_array = s.find_peaks(lazy_result=False, show_progressbar=False)
-mt.add_peak_array_to_signal_as_markers(s, peak_array)
+s.add_peak_array_as_markers(peak_array, color='purple', size=18)
 s.plot()
 fig_peaks = s._plot.signal_plot.figure
 fig_peaks.savefig("cbed_with_peaks.jpg")
