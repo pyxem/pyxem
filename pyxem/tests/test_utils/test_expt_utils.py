591e4314df14b1b79b3a3b04f7de06d9bc369311
Duncan Johnstone <duncanjohnstone@live.co.uk>
2019-04-18 12:55:10 +0100

    remove unused import

--- pyxem/tests/test_utils/test_expt_utils.py
+++ pyxem/tests/test_utils/test_expt_utils.py
@@ -25,8 +25,7 @@ from pyxem.utils.expt_utils import _index_coords, _cart2polar, _polar2cart, \
     radial_average, gain_normalise, remove_dead, affine_transformation, \
     regional_filter, subtract_background_dog, subtract_background_median, \
     subtract_reference, circular_mask, reference_circle, \
-    find_beam_offset_cross_correlation, calc_radius_with_distortion, \
-    call_ring_pattern, peaks_as_gvectors
+    find_beam_offset_cross_correlation, peaks_as_gvectors
 
 
 @pytest.fixture(params=[
