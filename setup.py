902e74438d49a15dfc18cc292172ba24b0141383
Duncan Johnstone <duncanjohnstone@live.co.uk>
2019-07-17 16:03:09 +0100

    updating skimage and matplotlib requires to latest

--- setup.py
+++ setup.py
@@ -47,13 +47,13 @@ setup(
     packages=find_packages(),
     # adjust the tabbing
     install_requires=[
-    	'scikit-image == 0.15.0', # See pyxem/pull/378
-      'matplotlib < 3.1.0' , # See pyxem/pull/403
+      'scikit-image >= 0.15.0', # See pyxem/pull/378
+      'matplotlib >= 3.1.1' , # See pyxem/pull/403
       'scikit-learn >= 0.19', # bug unknown
-     	'hyperspy >= 1.3',      # 1.2 fails, (NTU Workshop - May 2019) 
-      'transforms3d',        
+      'hyperspy >= 1.3',      # 1.2 fails, (NTU Workshop - May 2019)
+      'transforms3d',
       'diffpy.structure >= 3.0.0' # First Python 3 support
-      ],                       
+      ],
     package_data={
         "": ["LICENSE", "readme.rst",],
         "pyxem": ["*.py"],
