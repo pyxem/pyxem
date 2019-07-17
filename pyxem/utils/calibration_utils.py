8a586b49588c81f091b1b4d9e4869f13ce6010e3
Duncan Johnstone <duncanjohnstone@live.co.uk>
2019-07-17 16:53:00 +0100

    remove angle calculation utils and now using Line2DROI.angle

--- pyxem/utils/calibration_utils.py
+++ pyxem/utils/calibration_utils.py
@@ -185,21 +185,3 @@ def generate_ring_pattern(image_size, mask=False, mask_radius=10, scale=100,
         generated_pattern[maskROI > 0] *= 0
 
     return generated_pattern
-
-def unit_vector(vector):
-    """ Returns the unit vector of the vector.  """
-    return vector / np.linalg.norm(vector)
-
-def angle_between(v1, v2):
-    """ Returns the angle in radians between vectors 'v1' and 'v2'::
-
-            >>> angle_between((1, 0, 0), (0, 1, 0))
-            1.5707963267948966
-            >>> angle_between((1, 0, 0), (1, 0, 0))
-            0.0
-            >>> angle_between((1, 0, 0), (-1, 0, 0))
-            3.141592653589793
-    """
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
