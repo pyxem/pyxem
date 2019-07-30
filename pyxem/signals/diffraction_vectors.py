b94e3cc85380450f01e1b41ae25a23f3c67617dd
Tina Bergh <tina.bergh@ntnu.no>
2019-07-30 15:59:02 +0200

    Update plot_diffraction_vectors to also plot clusters.

--- pyxem/signals/diffraction_vectors.py
+++ pyxem/signals/diffraction_vectors.py
@@ -22,7 +22,9 @@ from hyperspy.signals import BaseSignal, Signal1D
 from hyperspy.api import markers
 
 import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
 from sklearn.cluster import DBSCAN
+from scipy.spatial import distance_matrix
 
 from pyxem.signals import push_metadata_through
 from pyxem.signals import transfer_navigation_axes
@@ -68,18 +70,46 @@ class DiffractionVectors(BaseSignal):
         self.hkls = None
 
     def plot_diffraction_vectors(self, xlim=1.0, ylim=1.0,
-                                 distance_threshold=0.01):
+                                 unique_vectors=None,
+                                 distance_threshold=0, min_samples=1,
+                                 image_to_plot_on=None,
+                                 image_cmap='gray',
+                                 plot_label_colors=False,
+                                 distance_threshold_all=0.05):
         """Plot the unique diffraction vectors.
 
         Parameters
         ----------
         xlim : float
-            The maximum x coordinate in reciprocal Angstroms to be plotted.
+            The maximum x coordinate to be plotted.
         ylim : float
-            The maximum y coordinate in reciprocal Angstroms to be plotted.
-        distance_threshold : float
-            The minimum distance in reciprocal Angstroms between diffraction
-            vectors to be passed to get_unique_vectors.
+            The maximum y coordinate to be plotted.
+        unique_vectors : DiffractionVectors, optional
+            The unique vectors to be plotted (optional). If not given, the
+            unique vectors will be found by get_unique_vectors.
+        distance_threshold : float, optional
+            The minimum distance, in calibrated units, between diffraction
+            vectors for them to be considered unique diffraction vectors.
+            Will be passed to get_unique_vectors if no unique vectors were
+            given.
+        min_samples : int, optional
+            The minimum number of vectors within one cluster for it to be
+            considered a core sample, i.e. to not be considered noise. Will
+            be passed to get_unique_vectors if no unique vectors were given.
+        image_to_plot_on : BaseSignal, optional
+            If provided, the vectors will be plotted on top of this image.
+            The image must be calibrated in terms of offset and scale.
+        image_cmap : string, optional
+            The color map to plot the image in.
+        plot_label_colors : bool, optional
+            If True (default is False), the vectors constituting each
+            cluster will be plotted also, with colors according to their
+            cluster membership. If True, the unique vectors will be
+            calculated by get_unique_vectors.
+        distance_threshold_all : float, optional
+            The minimum distance, in calibrated units, between diffraction
+            vectors inside one cluster for them to be plotted. Only used if
+            plot_label_colors is True.
 
         Returns
         -------
@@ -87,15 +117,64 @@ class DiffractionVectors(BaseSignal):
             The plot as a matplotlib figure.
 
         """
-        # Find the unique gvectors to plot.
-        unique_vectors = self.get_unique_vectors(distance_threshold)
-        # Plot the gvector positions
+
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.plot(unique_vectors.data.T[0], -unique_vectors.data.T[1], 'ro')
-        ax.set_xlim(-xlim, xlim)
-        ax.set_ylim(-ylim, ylim)
-        ax.set_aspect('equal')
+        offset, scale = 0., 1.
+        if image_to_plot_on is not None:
+            offset = image_to_plot_on.axes_manager[-1].offset
+            scale = image_to_plot_on.axes_manager[-1].scale
+            ax.imshow(image_to_plot_on, cmap=image_cmap)
+        else:
+            ax.set_xlim(-xlim, xlim)
+            ax.set_ylim(ylim, -ylim)
+            ax.set_aspect('equal')
+
+        if plot_label_colors is True:
+            unique_vectors, clusters = self.get_unique_vectors(
+                distance_threshold, min_samples, return_clusters=True)
+            labs = clusters.labels_[clusters.core_sample_indices_]
+            # Get all vectors from the clustering not considered noise
+            cores = clusters.components_
+            peaks = DiffractionVectors(cores)
+            peaks.axes_manager.set_signal_dimension(1)
+            # Since this original number of vectors can be huge, we
+            # find a reduced number of vectors that should be plotted, by
+            # running a new clustering on all the vectors not considered
+            # noise, considering distance_threshold_all.
+            peaks = peaks.get_unique_vectors(
+                distance_threshold_all, min_samples=1,
+                return_clusters=False)
+            peaks_all_len = peaks.data.shape[0]
+            labels_to_plot = np.zeros(peaks_all_len)
+            peaks_to_plot = np.zeros((peaks_all_len, 2))
+            # Find the labels of each of the peaks to plot by referring back
+            # to the list of labels for the original vectors.
+            for n, peak in zip(np.arange(peaks_all_len), peaks):
+                index = distance_matrix([peak.data], cores).argmin()
+                peaks_to_plot[n] = cores[index]
+                labels_to_plot[n] = labs[index]
+            # Assign a color value to each label, and shuffle these so that
+            # adjacent clusters hopefully get distinct colors.
+            cmap_lab = get_cmap('gist_rainbow')
+            lab_values_shuffled = np.arange(np.max(labels_to_plot) + 1)
+            np.random.shuffle(lab_values_shuffled)
+            labels_steps = np.array(list(map(
+                lambda n: lab_values_shuffled[int(n)], labels_to_plot)))
+            labels_steps = labels_steps / (np.max(labels_to_plot) + 1)
+            # Plot all peaks
+            for lab, peak in zip(labels_steps, peaks_to_plot):
+                ax.plot((peak[0] - offset) / scale,
+                        (peak[1] - offset) / scale, '.',
+                        color=cmap_lab(lab))
+        if unique_vectors is None:
+            unique_vectors = self.get_unique_vectors(
+                distance_threshold, min_samples)
+        # Plot the unique vectors
+        ax.plot((unique_vectors.data.T[0] - offset) / scale,
+                (unique_vectors.data.T[1] - offset) / scale, 'kx')
+        plt.tight_layout()
+        plt.axis('off')
         return fig
 
     def plot_diffraction_vectors_on_signal(self, signal, *args, **kwargs):
