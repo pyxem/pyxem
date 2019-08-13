40db945365f962f488e78b8aabfc9ba52fbc44c3
pc494 <pc494@cam.ac.uk>
2019-08-13 16:53:22 +0100

    expanding to also testing the trace

--- pyxem/tests/test_signals/test_strain_map.py
+++ pyxem/tests/test_signals/test_strain_map.py
@@ -45,35 +45,35 @@ def test_something_changes(Displacement_Grad_Map):
     assert not np.allclose(oneone_strain_original.data,oneone_strain_alpha.data, atol=0.01)
 
 
-@pytest.mark.skip(reason="Failing test above")
-def test_rotation(Displacement_Grad_Map):  # pragma: no cover
+def test_rotation(Displacement_Grad_Map):
     """
     We should always measure the same rotations, regardless of basis
     """
     local_D  = Displacement_Grad_Map
-    original = local_D.get_strain_maps().inav[3].data
-    local_D.rotate_strain_basis([1.3,+1.9]) #works in place
-    rotation_alpha =  local_D.get_strain_maps().inav[3].data
-    local_D = Displacement_Grad_Map
-    local_D.rotate_strain_basis([1.7,-0.3])
-    rotation_beta = local_D.get_strain_maps().inav[3].data
+    original = local_D.get_strain_maps()
+    rotation_alpha = original.rotate_strain_basis([1.3,+1.9])
+    rotation_beta  = original.rotate_strain_basis([1.7,-0.3])
 
     # check the functionality has left invarient quantities invarient
-    np.testing.assert_almost_equal(original, rotation_alpha, decimal=2)  # rotations
-    np.testing.assert_almost_equal(original, rotation_beta, decimal=2)  # rotations
+    np.testing.assert_almost_equal(original.inav[3].data, rotation_alpha.inav[3].data, decimal=2)  # rotations
+    np.testing.assert_almost_equal(original.inav[3].data, rotation_beta.inav[3].data, decimal=2)  # rotations
 
 
-@pytest.mark.skip(reason="basis change functionality not yet implemented")
-def test_trace(xy_vectors, right_handed, multi_vector):  # pragma: no cover
+def test_trace(Displacement_Grad_Map):
     """
     Basis does effect strain measurement, but we can simply calculate suitable invarients.
     See https://en.wikipedia.org/wiki/Infinitesimal_strain_theory for details.
     """
-    np.testing.assert_almost_equal(
-        np.add(
-            xy_vectors.inav[0].data, xy_vectors.inav[1].data), np.add(
-            right_handed.inav[0].data, right_handed.inav[1].data), decimal=2)
-    np.testing.assert_almost_equal(
-        np.add(
-            xy_vectors.inav[0].data, xy_vectors.inav[1].data), np.add(
-            multi_vector.inav[0].data, multi_vector.inav[1].data), decimal=2)
+    
+    local_D  = Displacement_Grad_Map
+    original = local_D.get_strain_maps()
+    rotation_alpha = original.rotate_strain_basis([1.3,+1.9])
+    rotation_beta  = original.rotate_strain_basis([1.7,-0.3])
+
+
+    np.testing.assert_almost_equal(np.add(original.inav[0].data, original.inav[1].data),
+                                   np.add(rotation_alpha.inav[0].data, rotation_alpha.inav[1].data),
+                                   decimal=2)
+    np.testing.assert_almost_equal(np.add(original.inav[0].data, original.inav[1].data),
+                                   np.add(rotation_beta.inav[0].data, rotation_beta.inav[1].data),
+                                   decimal=2)
