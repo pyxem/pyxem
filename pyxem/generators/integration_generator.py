486973412a5ad3278600267b3b697f41f092ac59
Stef Smeets <s.smeets@tudelft.nl>
2019-08-28 15:40:53 +0200

    Update integration generator to a working state

--- pyxem/generators/integration_generator.py
+++ pyxem/generators/integration_generator.py
@@ -21,18 +21,50 @@ Generating subpixel resolution on diffraction vectors.
 """
 
 import numpy as np
+from hyperspy.signals import BaseSignal
+from skimage import morphology
 
 from pyxem.signals.diffraction_vectors import DiffractionVectors
-from pyxem.utils.expt_utils import peaks_as_gvectors
-from pyxem.utils.subpixel_refinements_utils import _conventional_xc
-from pyxem.utils.subpixel_refinements_utils import get_experimental_square
-from pyxem.utils.subpixel_refinements_utils import get_simulated_disc
 
 import warnings
 
 
-class OrientationRefinementGenerator():
-    """Generates subpixel refinement of DiffractionVectors.
+def _get_intensities(z, vectors, center=None, radius=1):
+    """
+    Basic intensity integration routine, takes the maximum value at the
+    given vector positions with the number of pixels given by `radius`.
+
+    Parameters
+    ----------
+    vectors : DiffractionVectors
+        Vectors to the locations of the spots to be
+        integrated.
+    radius: int,
+        Number of pixels within which to find the largest maximum
+
+    Returns
+    -------
+    intensities : np.array
+        List of extracted intensities
+    """
+    if center is None:
+        center = np.array(z.shape) / 2
+    
+    i, j = (np.array(vectors.data) + center).astype(int).T
+    
+    if radius > 1:
+        footprint = morphology.disk(radius)
+        filtered = ndimage.maximum_filter(z, footprint=footprint)
+        intensities = filtered[j, i].reshape(-1,1)  # note that the indices are flipped
+    else:
+        intensities = z[j, i].reshape(-1,1)  # note that the indices are flipped
+    
+    return np.array(intensities)
+
+
+class IntegrationGenerator():
+    """
+    Integrates reflections at the given vector positions.
 
     Parameters
     ----------
@@ -40,15 +72,10 @@ class OrientationRefinementGenerator():
         The electron diffraction patterns to be refined
     vectors : DiffractionVectors | ndarray
         Vectors (in calibrated units) to the locations of the spots to be
-        refined. If given as DiffractionVectors, it must have the same
+        integrated. If given as DiffractionVectors, it must have the same
         navigation shape as the electron diffraction patterns. If an ndarray,
         the same set of vectors is mapped over all electron diffraction
         patterns.
-
-    References
-    ----------
-    [1] Pekin et al. Ultramicroscopy 176 (2017) 170-176
-
     """
 
     def __init__(self, dp, vectors):
@@ -59,27 +86,31 @@ class OrientationRefinementGenerator():
         self.calibration = [sig_ax[0].scale, sig_ax[1].scale]
         self.center = [sig_ax[0].size / 2, sig_ax[1].size / 2]
 
-    def get_intensities(img, result, projector, radius=1):
+    def extract_intensities(self, radius: int=2):
         """
-        Grab reflection intensities at given projection
+        Basic intensity integration routine, takes the maximum value at the
+        given vector positions with the number of pixels given by `radius`.
     
-        radius: int, optional
-            Search for largest point in defined radius around projected peak positions
+        Parameters
+        ----------
+        vectors : DiffractionVectors
+            Vectors to the locations of the spots to be
+            integrated.
+        radius: int,
+            Number of pixels within which to find the largest maximum
+    
+        Returns
+        -------
+        intensities : :obj:`hyperspy.signals.BaseSignal`
+            List of extracted intensities
         """
-        proj = projector.get_projection(result.alpha, result.beta, result.gamma)
-        i, j, hkl = get_indices(proj[:,3:5], result.scale, (result.center_x, result.center_y), img.shape, hkl=proj[:,0:3])
-        if radius > 1:
-            footprint = morphology.disk(radius)
-            img = ndimage.maximum_filter(img, footprint=footprint)
-        inty = img[i, j].reshape(-1,1)
-        return np.hstack((hkl, inty, np.ones_like(inty))).astype(int)
+        intensities = self.dp.map(_get_intensities, 
+                                  vectors=self.vectors_init, 
+                                  center=self.center, 
+                                  radius=radius, 
+                                  inplace=False)
 
-    def get_indices(pks, scale, center, shape, hkl=None):
-        """Get the pixel indices for an image"""
-        shapex, shapey = shape
-        i, j = (pks * scale + center).astype(int).T
-        sel = (0 < j) & (j < shapey) & (0 < i) & (i < shapex)
-        if hkl is None:
-            return i[sel], j[sel]
-        else:
-            return i[sel], j[sel], hkl[sel] 
+        intensities = BaseSignal(intensities)
+        intensities.axes_manager.set_signal_dimension(0)
+
+        return intensities
