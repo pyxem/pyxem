49bbc59694d5835d701a217c542ad76850e93920
Stef Smeets <s.smeets@tudelft.nl>
2019-08-30 18:06:25 +0200

    Implement summation method for integration

--- pyxem/generators/integration_generator.py
+++ pyxem/generators/integration_generator.py
@@ -23,6 +23,7 @@ Generating subpixel resolution on diffraction vectors.
 import numpy as np
 from hyperspy.signals import BaseSignal
 from skimage import morphology
+from skimage.measure import label   
 from scipy import ndimage as ndi
 
 from pyxem.signals.diffraction_vectors import DiffractionVectors
@@ -59,6 +60,113 @@ def _get_intensities(z, vectors, radius=1):
         intensities = z[j, i].reshape(-1,1)  # note that the indices are flipped
     
     return np.array(intensities)
+
+
+def _get_largest_connected_region(segmentation):
+    """
+    Take a binary segmentation image and return the largest connected
+    area.
+    """
+    labels = label(segmentation)
+    largest = np.argmax(np.bincount(labels.flat, weights=segmentation.flat))
+    return (labels == largest).astype(int)
+
+
+def _get_intensities_summation_method(z,
+                                      vectors,
+                                      box_inner: int=7,
+                                      box_outer: int=10,
+                                      n_min: int=5,
+                                      n_max: int=1000,
+                                      snr_thresh=3.0,
+                                      verbose: bool=False):
+    """
+    Integrate reflections using the summation method. Two boxes are defined, the
+    inner box is used to define the integration area. The outer box is used 
+    to calculate the average signal-to-noise ratio (SNR). 
+    All pixels with a large enough SNR are considered to be signal. The largest region 
+    of connected signal pixels are summed to calculate the reflection intensity.
+
+    Parameters
+    ----------
+
+    vectors : DiffractionVectors
+        Vectors to the locations of the spots to be
+        integrated.
+    box_inner : int
+        Defines the radius (size) of the inner box, which must be larger than the reflection.
+        The total box size is 2*box_inner
+    box_outer : int
+        Defines radius (size) of the outer box. The total box size is 2*box_inner
+        The border between the inner and outer box is considered background 
+        and used to calculate the (SNR) for each pixel: SNR = (I - <I>/std(I_bkg)).
+    snr_thresh : float
+        Minimum signal-to-noise for a pixel to be considered as `signal`. 
+    n_min: int
+        If the number of SNR pixels in the inner box < n_min, the reflection is discared
+    n_max:
+        If the number of SNR pixels in the inner box > n_max, the reflection is discareded
+    verbose : bool
+        Print statistics for every reflection (for debugging)
+
+    Returns
+    -------
+    peaks : np.array
+        Array with 4 columns: X-position, Y-position, intensity, reflection SNR
+
+    Implementation based on Barty et al, J. Appl. Cryst. (2014). 47, 1118-1131
+                            Lesli, Acta Cryst. (2006). D62, 48-57
+    """
+    # prepare meshgrid for centroid calculation
+    meshi, meshj = np.meshgrid(np.arange(box_inner*2), np.arange(box_inner*2))
+    meshi -= box_inner
+    meshj -= box_outer
+    
+    peaks = []
+    
+    for i, j in vectors:
+        box = z[j-box_inner:j+box_inner, i-box_inner:i+box_inner].copy()
+    
+        bkg = np.hstack([
+            z[j-box_outer:j+box_outer, i-box_outer:i-box_inner].ravel(),
+            z[j-box_outer:j+box_outer, i+box_inner:i+box_outer].ravel(),
+            z[j-box_outer:j-box_inner, i-box_inner:i+box_inner].ravel(),
+            z[j+box_inner:j+box_outer, i-box_inner:i+box_inner].ravel()])
+    
+        bkg_mean = bkg.mean()
+        bkg_std = bkg.std()
+        box_snr = (box - bkg_mean) / bkg_std
+        
+        # get mask for signal (I > SNR)
+        signal_mask = _get_largest_connected_region(box_snr > snr_thresh)
+        
+        n_pix = signal_mask.sum()
+        signal = (box - bkg_mean)*signal_mask
+        inty = signal.sum()
+        snr = (inty/n_pix) / bkg_std
+        
+        # calculate centroid
+        dX = np.average(meshi, weights=signal)
+        dY = np.average(meshj, weights=signal)
+        X = i + dX
+        Y = j + dY
+        
+        if verbose:
+            print(f"\nMean(I): {bkg_mean:.2f} | Std(I): {bkg_std:.2f} | n_pix: {n_pix} \n" \
+                  f"I: {inty:.2f} | I/pix: {inty/n_pix:.2f} | SNR(I): {snr:.2f} \n" \
+                  f"i: {i:.2f} | j: {j:.2f} | dX: {dX:.2f} | dY: {dY:.2f} | X: {X:.2f} | Y: {Y:.2f}")
+        
+        if n_pix > n_max:
+            continue
+        if n_pix < n_min:
+            continue
+        
+        peaks.append([X, Y, inty, snr])
+    
+    # TODO: how best to store these data?
+    peaks = np.array(peaks)
+    intensities = peaks[:,2]
+    return intensities.reshape(-1,1)
 
 
 class IntegrationGenerator():
@@ -112,6 +220,59 @@ class IntegrationGenerator():
                                   radius=radius, 
                                   inplace=False)
 
+        intensities = BaseSignal(intensities)
+        intensities.axes_manager.set_signal_dimension(0)
+
+        return intensities
+
+    def extract_intensities_summation_method(self,
+                                             box_inner: int=7,
+                                             box_outer: int=10,
+                                             n_min: int=5,
+                                             n_max: int=1000,
+                                             snr_thresh: float=3.0):
+        """
+        Integrate reflections using the summation method. Two boxes are defined, the
+        inner box is used to define the integration area. The outer box is used 
+        to calculate the average signal-to-noise ratio (SNR). 
+        All pixels with a large enough SNR are considered to be signal. The largest region 
+        of connected signal pixels are summed to calculate the reflection intensity.
+    
+        Parameters
+        ----------
+    
+        box_inner : int
+            Defines the size of the inner box, which must be larger than the reflection.
+        box_outer : int
+            Defines the size of the outer box. The border between the inner and outer
+            box is considered background and used to calculate the (SNR) for each 
+            pixel: SNR = (I - <I>/std(I_bkg)).
+        snr_thresh : float
+            Minimum signal-to-noise for a pixel to be considered as `signal`. 
+        n_min: int
+            If the number of SNR pixels in the inner box < n_min, the reflection is discared
+        n_max:
+            If the number of SNR pixels in the inner box > n_max, the reflection is discareded
+        verbose : bool
+            Print statistics for every reflection (for debugging)
+    
+        Returns
+        -------
+        peaks : np.array
+            Array with 4 columns: X-position, Y-position, intensity, reflection SNR
+    
+        Implementation based on Barty et al, J. Appl. Cryst. (2014). 47, 1118-1131
+                                Lesli, Acta Cryst. (2006). D62, 48-57
+        """
+        intensities = self.dp.map(_get_intensities_summation_method, 
+                                  vectors=self.vector_pixels, 
+                                  box_inner=box_inner,
+                                  box_outer=box_outer,
+                                  n_min=n_min,
+                                  n_max=n_max,
+                                  snr_thresh=snr_thresh,
+                                  inplace=False)
+        
         intensities = BaseSignal(intensities)
         intensities.axes_manager.set_signal_dimension(0)
 
