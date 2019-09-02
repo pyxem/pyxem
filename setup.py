c3999269091a9e56b9f2194c2d44177a1421407a
pc494 <pc494@cam.ac.uk>
2019-09-02 14:16:56 +0100

    pin hyperspy below 1.5

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
       'scikit-image >= 0.15.0',   # exclude_border argument in peak_finder laplacian (PR #436)
       'matplotlib >= 3.1.1' ,     # 3.1.0 failed
       'scikit-learn >= 0.19',     # reason unknown
-      'hyperspy >= 1.5',          # 1.2 fails, (NTU Workshop - May 2019)
+      'hyperspy <= 1.4',          # 1.2 fails, (NTU Workshop - May 2019)
       'diffsims',
       'numpy < 1.17.0'            # 3.7 appveyor test failed on 1.17.1
       ],
