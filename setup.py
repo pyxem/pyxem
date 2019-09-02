060d84c3a237a7e17053c8ebcc8d901b807e70f6
Duncan N. Johnstone <duncanjohnstone@live.co.uk>
2019-09-02 19:16:13 +0100

    numpy == 1.16.5

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
       'scikit-learn >= 0.19',     # reason unknown
       'hyperspy >= 1.3',          # 1.2 fails, (NTU Workshop - May 2019)
       'diffsims',
-      'numpy == 1.16.*'           # See PR#464 and Issue#466
+      'numpy == 1.16.5'           # See PR#464 and Issue#466
       ],
     package_data={
         "": ["LICENSE", "readme.rst",],
