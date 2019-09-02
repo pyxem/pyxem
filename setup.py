66d46ef85b0650e6d6f3d3acf92628345624f7b6
pc494 <pc494@cam.ac.uk>
2019-09-02 16:26:38 +0100

    pinning numpy version to ==1.16.*

--- setup.py
+++ setup.py
@@ -50,9 +50,9 @@ setup(
       'scikit-image >= 0.15.0',   # exclude_border argument in peak_finder laplacian (PR #436)
       'matplotlib >= 3.1.1' ,     # 3.1.0 failed
       'scikit-learn >= 0.19',     # reason unknown
-      'hyperspy <= 1.4',          # 1.2 fails, (NTU Workshop - May 2019)
+      'hyperspy >= 1.3',          # 1.2 fails, (NTU Workshop - May 2019)
       'diffsims',
-      'numpy < 1.17.0'            # 3.7 appveyor test failed on 1.17.1
+      'numpy == 1.16.*'           # See PR#464 and Issue#466
       ],
     package_data={
         "": ["LICENSE", "readme.rst",],
