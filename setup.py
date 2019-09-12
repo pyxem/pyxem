2a64545d03ebea6ef3db330e8e88ecdd3330528f
Magnus Nord <magnunor@gmail.com>
2019-09-12 14:34:47 +0200

    setup.py: change matplotlib version to 3.1.0 or later
    Due to bug before 3.1.0 leading to segfaults when generating images
    in the user guide documentation

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'numpy>=1.13',
             'h5py',
             'ipython>=2.0',
-            'matplotlib>=2.0',
+            'matplotlib>=3.1.0',
             'hyperspy>=1.4',
             'dask',
             'tqdm',
