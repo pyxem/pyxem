Changed multifit procedure, fixed a bug with reduced intensity normalisation, and added documentation references.

pyxem/components/scattering_fit_component_lobato.py
@@ -38,27 +38,38 @@ class ScatteringFitComponentLobato(Component):
 
         N and C are fitting parameters for the Component class.
 
-        Fit to Lobato & Van Dyck (2014)
+        The parametrisation is detailed in [1].
 
         Parameters
         ----------
         elements: list of str
-                    A list of elements present (by symbol).
+            A list of elements present (by symbol).
         fracs: list of float
-                    A list of fraction of the respective elements. Should sum to 1.
+            A list of fraction of the respective elements. Should sum to 1.
         N : float
-                    The "slope" of the fit.
+            The "slope" of the fit.
         C : float
-                    An additive constant to the fit.
+            An additive constant to the fit.
+
+        References
+        ----------
+        [1] Lobato, I., & Van Dyck, D. (2014). An accurate parameterization for
+        scattering factors, electron densities and electrostatic potentials for
+        neutral atoms that obey all physical constraints. Acta Crystallographica
+        Section A: Foundations and Advances, 70(6), 636-649.
 
         """
         Component.__init__(self, ['N', 'C'])
+        self._whitelist['elements'] = ('init,sig', elements)
+        self._whitelist['fracs'] = ('init,sig', fracs)
         self.elements = elements
         self.fracs = fracs
         params = []
         for e in elements:
             params.append(ATOMIC_SCATTERING_PARAMS_LOBATO[e])
         self.params = params
+        self.N.value = N
+        self.C.value = C
 
     def function(self, x):
         """
@@ -86,6 +97,6 @@ class ScatteringFitComponentLobato(Component):
             sum_squares += np.square(fi) * elem_frac
             square_sum += fi * elem_frac
 
-        self.square_sum = N * np.square(square_sum)
+        self.square_sum = np.square(square_sum)
         # square sum is kept for normalisation.
         return N * sum_squares + C

pyxem/components/scattering_fit_component_xtables.py
@@ -38,34 +38,43 @@ class ScatteringFitComponentXTables(Component):
 
         N and C are fitting parameters for the Component class.
 
-        Fit to International Tables Vol. C, table 4.3.2.3
+        The fitted scattering factor is from [1].
 
         Parameters
         ----------
         elements: list of str
-                    A list of elements present (by symbol).
+            A list of elements present (by symbol).
         fracs: list of float
-                    A list of fraction of the respective elements. Should sum to 1.
+            A list of fraction of the respective elements. Should sum to 1.
         N : float
-                    The "slope" of the fit.
+            The "slope" of the fit.
         C : float
-                    An additive constant to the fit.
+            An additive constant to the fit.
+
+        References
+        ----------
+        [1] Prince, E. (2004). International tables for crystallography.
+        Vol. C, table 4.3.2.3.
 
         """
         Component.__init__(self, ['N', 'C'])
+        self._whitelist['elements'] = ('init,sig', elements)
+        self._whitelist['fracs'] = ('init,sig', fracs)
         self.elements = elements
         self.fracs = fracs
         params = []
         for e in elements:
             params.append(ATOMIC_SCATTERING_PARAMS[e])
         self.params = params
+        self.N.value = N
+        self.C.value = C
 
     def function(self, x):
         """
         The actual fitted function. Based on the HyperSpy Component class.
         The function "function" is called innately by HyperSpy's fitting
         algorithm, and represents the appropriate atomic scattering factor
-        function presented in International Tables Vol. C, table 4.3.2.3
+        function.
 
         """
         N = self.N.value
@@ -84,6 +93,6 @@ class ScatteringFitComponentXTables(Component):
             sum_squares += np.square(fi) * elem_frac
             square_sum += fi * elem_frac
 
-        self.square_sum = N * np.square(square_sum)
+        self.square_sum = np.square(square_sum)
         # square sum is kept for normalisation.
         return N * sum_squares + C

pyxem/generators/red_intensity_generator1d.py
@@ -30,15 +30,12 @@ from pyxem.signals.reduced_intensity1d import ReducedIntensity1D
 
 from pyxem.components.scattering_fit_component_xtables import ScatteringFitComponentXTables
 from pyxem.components.scattering_fit_component_lobato import ScatteringFitComponentLobato
-from pyxem.utils.ri_utils import scattering_to_signal_lobato, \
-    scattering_to_signal_xtables, subtract_pattern, mask_from_pattern
+from pyxem.utils.ri_utils import subtract_pattern, mask_from_pattern
 from pyxem.signals import transfer_navigation_axes
 from pyxem.signals import transfer_signal_axes
 
 scattering_factor_dictionary = {'lobato': ScatteringFitComponentLobato,
                                 'xtables': ScatteringFitComponentXTables}
-scattering_signal_dictionary = {'lobato': scattering_to_signal_lobato,
-                                'xtables': scattering_to_signal_xtables}
 
 
 class ReducedIntensityGenerator1D():
@@ -75,7 +72,7 @@ class ReducedIntensityGenerator1D():
         """
         self.signal.axes_manager.signal_axes[0].scale = calibration
 
-    def set_cutoff_vector(self, s_min, s_max):
+    def set_s_cutoff(self, s_min, s_max):
         """
         Scattering vector cutoff for the purposes of fitting an atomic scattering
         factor to the 1D profile. Specified in terms of s (in inverse angstroms).
@@ -94,12 +91,12 @@ class ReducedIntensityGenerator1D():
 
     def fit_atomic_scattering(self, elements, fracs,
                               N=1., C=0., scattering_factor='lobato',
-                              plot_fit=True):
+                              plot_fit=True, *args, **kwargs):
         """Fits a diffraction intensity profile to the background using
         FIT = N * sum(ci * (fi^2) + C)
 
         The cutoff for the scattering factor fit to s is defined via the function
-        set_cutoff_vector above.
+        set_s_cutoff above.
 
         Parameters
         ----------
@@ -110,15 +107,21 @@ class ReducedIntensityGenerator1D():
                     A list of fraction of the respective elements. Should sum to 1.
                     Example: [0.2, 0.2, 0.6] (for CaCO3)
         N : float
-                    The "slope" of the fit.
+                    The "slope" of the fit. Initial value is used to start the
+                    fit.
         C : float
-                    An additive constant to the fit.
+                    An additive constant to the fit. Initial value is used to
+                    start the fit.
         scattering_factor : str
                     Type of scattering parameters fitted. Default is lobato.
                     See scattering_fit_component for more details.
         plot_fit: bool
                     A bool to decide if the fit from scattering is plotted
                     after fitting.
+        *args:
+            Arguments to be passed to hs.multifit().
+        **kwargs:
+            Keyword arguments to be passed to hs.multifit().
         """
 
         fit_model = self.signal.create_model()
@@ -127,16 +130,18 @@ class ReducedIntensityGenerator1D():
 
         fit_model.append(background)
         fit_model.set_signal_range(self.cutoff)
-        fit_model.multifit()
+        fit_model.multifit(*args, **kwargs)
         fit_model.reset_signal_range()
         if plot_fit == True:
             fit_model.plot()
-        C_values = background.C.as_signal()
+        fit = fit_model.as_signal()
+
         N_values = background.N.as_signal()
-        s_size = self.sig_size[0]
-        s_scale = self.signal.axes_manager.signal_axes[0].scale
-        fit, normalisation = scattering_signal_dictionary[scattering_factor](elements,
-                                    fracs, N_values, C_values, s_size, s_scale)
+        square_sum = background.square_sum
+
+        x_size = N_values.data.shape[0]
+        y_size = N_values.data.shape[1]
+        normalisation = N_values.data.reshape(x_size, y_size, 1) * square_sum
 
         self.normalisation = normalisation
         self.background_fit = fit
@@ -243,7 +248,7 @@ class ReducedIntensityGenerator1D():
                       dtype='float64')
         s *= self.signal.axes_manager.signal_axes[0].scale
 
-        reduced_intensity = (4 * np.pi * s *
+        reduced_intensity = (2 * np.pi * s *
                              np.divide((self.signal.data - self.background_fit),
                                        self.normalisation))
 

pyxem/tests/test_generators/test_red_intensity_generator.py
@@ -66,15 +66,19 @@ def test_fit_atomic_scattering(red_int_generator):
                               [44.18563327, 35.06613799, 25.7903345, 21.59942561,
                                19.48821693, 18.28295037, 17.56853016, 17.13988946,
                                16.87888168, 16.71651788]]])
-    norm_expected = np.array([[[1.24147037, 1.01779718, 0.72243479, 0.53822083, 0.41556406,
-                                0.32543231, 0.25752301, 0.206315, 0.16763856, 0.13822315],
-                               [2.48294075, 2.03559437, 1.44486959, 1.07644167, 0.83112813,
-                                0.65086462, 0.51504602, 0.41263001, 0.33527711, 0.27644629]],
+    norm_expected = np.array([[[ 6.95347555,  4.67360174,  2.35465087,  1.30692365,
+                                0.77912148,  0.47780484,  0.29919979,  0.19203962,
+                                0.12678767,  0.08619672],
+                                [13.9069511 ,  9.34720348,  4.70930174,  2.61384731,
+                                1.55824297,  0.95560969,  0.59839959,  0.38407924,
+                                0.25357535,  0.17239345]],
 
-                              [[3.72441111, 3.05339154, 2.16730438, 1.61466249, 1.24669219,
-                                0.97629693, 0.77256903, 0.61894501, 0.50291567, 0.41466944],
-                               [4.9658815, 4.07118874, 2.88973919, 2.15288333, 1.66225626,
-                                1.30172924, 1.03009204, 0.82526001, 0.67055423, 0.55289259]]])
+                                [[20.86042659, 14.02080517,  7.06395259,  3.92077095,
+                                2.33736445,  1.43341453,  0.89759938,  0.57611885,
+                                0.38036302,  0.25859017],
+                                [27.81390212, 18.6944069 ,  9.41860346,  5.2276946 ,
+                                3.11648593,  1.91121937,  1.19679917,  0.76815847,
+                                0.5071507 ,  0.3447869 ]]])
 
     assert np.allclose(red_int_generator.background_fit, fit_expected)
     assert np.allclose(red_int_generator.normalisation, norm_expected)
@@ -82,7 +86,7 @@ def test_fit_atomic_scattering(red_int_generator):
 
 def test_set_cutoff(red_int_generator):
     s_min, s_max = 0, 8
-    red_int_generator.set_cutoff_vector(s_min, s_max)
+    red_int_generator.set_s_cutoff(s_min, s_max)
     assert red_int_generator.cutoff == [s_min, s_max]
 
 
@@ -131,9 +135,11 @@ def test_get_reduced_intensity(red_int_generator):
     ri = red_int_generator.get_reduced_intensity()
     assert isinstance(ri, ReducedIntensity1D)
 
-    ri_expected_single = np.array([-0.,0.28825135,5.40069217,11.20803745,
-                                    13.64332079,8.28785318,-11.48095348,
-                                    -54.78616349,-133.1141455,-260.12362871])
+    ri_expected_single = np.array([-0.00000000e+00, 3.13870790e-02,
+                                    8.28498177e-01, 2.30786214e+00,
+                                    3.63850434e+00, 2.82242349e+00,
+                                    -4.94086192e+00, -2.94293632e+01,
+                                    -8.80017073e+01, -2.08564231e+02])
     ri_expected_array = np.vstack((ri_expected_single,ri_expected_single,
                                     ri_expected_single,ri_expected_single)).reshape(2,2,10)
     ri_expected = ReducedIntensity1D(ri_expected_array)