6881699f09ef42c74527dd6360b2171ac3b69d91
pc494 <pc494@cam.ac.uk>
2019-10-02 15:59:07 +0100

    Tests-misc now run cleanly

--- pyxem/tests/test_generators/test_subpixelrefinement_generator.py
+++ pyxem/tests/test_generators/test_subpixelrefinement_generator.py
@@ -53,22 +53,26 @@ class Test_subpixelpeakfinders():
     cases as well as confirming we have avoided 'off by one' errors """
 
     def create_spot(self):
-        z1 = np.zeros((128, 128))
-        z2 = np.zeros((128, 128))
+        z1,z1a = np.zeros((128, 128)),np.zeros((128, 128))
+        z2,z2a = np.zeros((128, 128)),np.zeros((128, 128))
 
-        for r in [4, 3, 2]:
-            c = 1 / r
-            rr, cc = draw.circle(30, 90, radius=r, shape=z1.shape)  # 30 is y!
-            z1[rr, cc] = c
-            z2[rr, cc] = c
-            rr2, cc2 = draw.circle(100, 60, radius=r, shape=z2.shape)
-            z2[rr2, cc2] = c
+        rr, cc = draw.circle(30, 90, radius=4, shape=z1.shape)  # 30 is y!
+        z1[rr, cc],z2[rr, cc] = 1,1
+        rr2, cc2 = draw.circle(100, 60, radius=4, shape=z2.shape)
+        z2[rr2, cc2] = 1
+        rr, cc = draw.circle(30, 90+3, radius=4, shape=z1.shape)  # 30 is y!
+        z1a[rr, cc],z2a[rr, cc] = 1,1
+        rr2, cc2 = draw.circle(100-2, 60, radius=4, shape=z2.shape)
+        z2a[rr2, cc2] = 1
 
-        #TODO: mark the centers for the local gaussian method
-        #TODO: add a slight shift to make z1a and z2a
-        dp = ElectronDiffraction2D(np.asarray([[z1, z1], [z2, z2]]))  # this needs to be in 2x2
+        #marks centers for local local_gaussian_method
+        z1[30,90],z2[30,90],z2[100,60] = 1.2,1.2,1.3
+        z1a[30,93],z2a[30,93],z2a[98,60] = 1.2,1.4,1.7
+
+        dp = ElectronDiffraction2D(np.asarray([[z1, z1a], [z2, z2a]]))  # this needs to be in 2x2
         return dp
 
+    @pytest.fixture
     def create_Diffraction_vectors(self):
         v1 = np.array([[90 - 64, 30 - 64]])
         v2 = np.array([[90 - 64, 30 - 64], [100 - 64, 60 - 64]])
@@ -76,50 +80,62 @@ class Test_subpixelpeakfinders():
         vectors.axes_manager.set_signal_dimension(0)
         return vectors
 
+    @pytest.fixture
     def create_numpy_vectors(self):
-        v = np.array([[90 - 64, 30 - 64]]
-        return v
+        return np.array([[90 - 64, 30 - 64]])
 
-    def test_assertioned_xc():
-        spr = SubpixelrefinementGenerator(dp, diffraction_vectors)
+    @pytest.mark.parametrize("diffraction_vectors",
+                            [create_Diffraction_vectors,create_numpy_vectors])
+    def test_assertioned_xc(diffraction_vectors):
+        spr = SubpixelrefinementGenerator(create_spot(), diffraction_vectors)
         s = spr.conventional_xc(12, 4, 8)
         error = s.data[0, 0] - np.asarray([[90 - 64, 30 - 64]])
         rms_error = np.sqrt(error[0, 0]**2 + error[0, 1]**2)
         assert rms_error < 0.2  # 1/5th a pixel
-
+    @pytest.mark.parametrize("diffraction_vectors",
+                            [create_Diffraction_vectors,create_numpy_vectors])
     def test_assertioned_com():
         # should be perfect detection for these cases
-        spr = SubpixelrefinementGenerator(dp, diffraction_vectors)
+        spr = SubpixelrefinementGenerator(create_spot(), diffraction_vectors)
         s = spr.center_of_mass_method(8)
         error = s.data[0, 0] - np.asarray([[90 - 64, 30 - 64]])
         rms_error = np.sqrt(error[0, 0]**2 + error[0, 1]**2)
         assert rms_error < 1e-5  # perfect detection for this trivial case
-
+    @pytest.mark.parametrize("diffraction_vectors",
+                            [create_Diffraction_vectors,create_numpy_vectors])
     def test_assertioned_log():
         #rename some of the internal bits of functionality
         pass
 
+#class Test_misc():
+""" These tests will be removed for 0.11.0, but are needed for the log method &
+security on the x/y conventions until then """
 
-class Test_misc():
-    """ These tests will be removed for 0.11.0, but are needed for the log method &
-    security on the x/y conventions until then """
-    
-    @pytest.mark.parametrize('dp, diffraction_vectors',
+def create_spot_gaussian():
+    z1 = np.zeros((128, 128))
+
+    x = np.arange(0.0, 10, 1.0)
+    y = x[:, np.newaxis]
+    z1[20:30, 50:60] = np.exp(-((x - 5.1)**2 + (y - 5.3)**2) / 4)
+    dp = ElectronDiffraction2D(np.asarray([[z1, z1], [z1, z1]]))  # this needs to be in 2x2
+    return dp
+
+@pytest.mark.parametrize('dp, diffraction_vectors',
                          [(create_spot_gaussian(), np.array([[55 - 64, 25 - 64]]))])
-    @pytest.mark.filterwarnings('ignore::UserWarning')  # our warning
-    def test_bad_square_size_local_gaussian_method(self,dp, diffraction_vectors):
-        spr = SubpixelrefinementGenerator(dp, diffraction_vectors)
-        s = spr.local_gaussian_method(2)
+@pytest.mark.filterwarnings('ignore::UserWarning')  # our warning
+def test_bad_square_size_local_gaussian_method(self,dp, diffraction_vectors):
+    spr = SubpixelrefinementGenerator(dp, diffraction_vectors)
+    s = spr.local_gaussian_method(2)
 
-    def test_xy_errors_in_conventional_xc_method_as_per_issue_490(self):
-        """ This was the MWE example code for the issue """
-        dp = get_simulated_disc(100,20)
-        # translate y by +4
-        shifted = np.pad(dp, ((0,4),(0,0)), 'constant')[4:].reshape(1,1,*dp.shape)
-        signal = ElectronDiffraction2D(shifted)
-        spg = SubpixelrefinementGenerator(signal, np.array([[0,0]]))
-        peaks = spg.conventional_xc(100,20,1).data[0,0,0] #as quoted in the issue
-        np.testing.assert_allclose([0,-4],peaks)
-        """ we also test com method for clarity """
-        peaks = spg.center_of_mass_method(60).data[0,0,0]
-        np.testing.assert_allclose([0,-4],peaks,atol=1.5)
+def test_xy_errors_in_conventional_xc_method_as_per_issue_490(self):
+    """ This was the MWE example code for the issue """
+    dp = get_simulated_disc(100,20)
+    # translate y by +4
+    shifted = np.pad(dp, ((0,4),(0,0)), 'constant')[4:].reshape(1,1,*dp.shape)
+    signal = ElectronDiffraction2D(shifted)
+    spg = SubpixelrefinementGenerator(signal, np.array([[0,0]]))
+    peaks = spg.conventional_xc(100,20,1).data[0,0,0] #as quoted in the issue
+    np.testing.assert_allclose([0,-4],peaks)
+    """ we also test com method for clarity """
+    peaks = spg.center_of_mass_method(60).data[0,0,0]
+    np.testing.assert_allclose([0,-4],peaks,atol=1.5)
