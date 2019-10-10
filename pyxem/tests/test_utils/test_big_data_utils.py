740ee4dc1ec4eb5270bf75236101e1be52c38322
pc494 <pc494@cam.ac.uk>
2019-10-10 12:53:29 +0100

    introduce big-data testing

---
+++ pyxem/tests/test_utils/test_big_data_utils.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2019 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+import hyperspy.api as hs
+import numpy as np
+import pytest
+import pyxem as pxm
+from pyxem.utils.big_data_utils import main_function
+import os
+
+@pytest.fixture()
+def big_electron_diffraction_pattern():
+    z = np.arange(0,96,step=1).reshape(6,4,2,2)
+    dp =  pxm.ElectronDiffraction2D(z)
+    dp.save('tempfile')
+    return dp
+
+def single_navigation_square_root(z):
+    return np.sqrt(z)
+
+
+def test_core_big_data_functionality(big_electron_diffraction_pattern):
+    expected_output = np.sqrt(big_electron_diffraction_pattern.data)
+
+    filepath = 'tempfile.hspy'
+    x_list = [0,2,4]
+    y_list = np.arange(0,3,2)
+
+    test_output = main_function(filepath,x_list,y_list,single_navigation_square_root)
+    assert np.allclose(expect_output,test_output.data)
+    os.remove('tempfile.hspy')
