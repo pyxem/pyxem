00a96445db18605854beb0185169900ef46b38cc
Phillip Crout <32455330+pc494@users.noreply.github.com>
2019-11-06 19:15:55 +0000

    [skip ci] clean up imports to a single line

--- pyxem/generators/subpixelrefinement_generator.py
+++ pyxem/generators/subpixelrefinement_generator.py
@@ -27,8 +27,7 @@ from skimage.feature import register_translation
 from pyxem.signals.diffraction_vectors import DiffractionVectors
 from pyxem.signals.diffraction_vectors import DiffractionVectors
 from pyxem.utils.expt_utils import peaks_as_gvectors
-from pyxem.signals import (transfer_signal_axes,
-                           transfer_navigation_axes)
+from pyxem.signals import transfer_signal_axes, transfer_navigation_axes
 from pyxem.utils.subpixel_refinements_utils import get_experimental_square
 from pyxem.utils.subpixel_refinements_utils import get_simulated_disc
 from pyxem.utils.subpixel_refinements_utils import _get_pixel_vectors
