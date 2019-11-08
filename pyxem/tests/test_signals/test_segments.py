754237248d9cfd6590a7eecbe3aa204720fd4667
Tina Bergh <tina.bergh@ntnu.no>
2019-11-08 15:09:28 +0100

    Add test instance for virtual diffraction from segments with only one vector each.

--- pyxem/tests/test_signals/test_segments.py
+++ pyxem/tests/test_signals/test_segments.py
@@ -196,7 +196,7 @@ class TestVDFSegment:
             corr_threshold=0.7, vector_threshold=0, segment_threshold=-1)
 
     @pytest.mark.xfail
-    def test_corelate_segments_bad_thresholds(self, vdf_segments: VDFSegment):
+    def test_correlate_segments_bad_thresholds(self, vdf_segments: VDFSegment):
         corrsegs = vdf_segments.correlate_vdf_segments(vector_threshold=4,
                                                        segment_threshold=5)
 
@@ -204,6 +204,13 @@ class TestVDFSegment:
                                               signal_data):
         corrsegs = vdf_segments.correlate_vdf_segments(0.1, 1, 1)
         vs = corrsegs.get_virtual_electron_diffraction(
+            calibration=1, sigma=1,
+            shape=signal_data.axes_manager.signal_shape)
+        assert isinstance(vs, ElectronDiffraction2D)
+
+    def test_get_virtual_electron_diffraction_for_single_vectors(
+            self, vdf_segments: VDFSegment, signal_data):
+        vs = vdf_segments.get_virtual_electron_diffraction(
             calibration=1, sigma=1,
             shape=signal_data.axes_manager.signal_shape)
         assert isinstance(vs, ElectronDiffraction2D)
