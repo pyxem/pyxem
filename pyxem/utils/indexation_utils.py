de3acdb90dd454920505d957a38694b0f6afe3b3
EirikOpheim <eirikoph@stud.ntnu.no>
2020-02-18 16:04:54 +0100

    Import signals function

--- pyxem/utils/indexation_utils.py
+++ pyxem/utils/indexation_utils.py
@@ -28,6 +28,7 @@ import numpy as np
 from pyxem.utils.expt_utils import _cart2polar
 from pyxem.utils.vector_utils import get_rotation_matrix_between_vectors
 from pyxem.utils.vector_utils import get_angle_cartesian
+from pyxem.signals import select_method_from_method_dict
 
 from transforms3d.euler import mat2euler, euler2mat
 from transforms3d.quaternions import mat2quat
