c02c017ea632a844899dade7818feb32ab7869ec
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-03-24 21:45:39 +0000

    update setup.py to merge pixstem and pyxem

--- pyxem/release_info.py
+++ pyxem/release_info.py
@@ -1,9 +1,9 @@
 name = "pyxem"
-version = "0.11.0dev"
-author = "Duncan Johnstone, Phillip Crout"
+version = "0.12.0"
+author = "Duncan Johnstone, Phillip Crout, Magnus Nord"
 copyright = "Copyright 2017-2020, The pyXem Developers"
-credits = ["Duncan Johnstone", "Phillip Crout", "Ben Martineau", "Simon Hogas"]
+credits = ["Duncan Johnstone", "Phillip Crout", "Magnus Nord", "Ben Martineau", "Simon Hogas"]
 license = "GPLv3"
-maintainer = "Duncan Johnstone, Phillip Crout"
+maintainer = "Duncan Johnstone, Phillip Crout, Magnus Nord"
 email = "pyxem.team@gmail.com"
 status = "Development"
--- setup.py
+++ setup.py
@@ -31,6 +31,11 @@ setup(
     license=license,
     url="https://github.com/pyxem/pyxem",
     long_description=open('README.rst').read(),
+    keywords = [
+        'STEM',
+        'data analysis',
+        'microscopy',
+        ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
@@ -53,49 +58,15 @@ setup(
         'hyperspy >= 1.5.2',        # earlier versions incompatible with numpy >= 1.17.0
         'diffsims > 0.2.1',        # Makes use of functionality introduced in this release
         'lmfit >= 0.9.12',
-        'pyfai'
+        'pyfai',
+        'ipython>=2.0',
+        'h5py',
+        'dask',
+        'tqdm'
     ],
     package_data={
         "": ["LICENSE", "readme.rst"],
         "pyxem": ["*.py", "hyperspy_extension.yaml"],
     },
     entry_points={'hyperspy.extensions': ['pyxem = pyxem']},
-)
-
-from setuptools import setup, find_packages
-
-with open('README.md') as f:
-    long_description = f.read()
-
-setup(
-        name = 'pixstem',
-        packages = find_packages(),
-        version = '0.4.1.dev1',
-        description = 'Library for processing scanning transmission electron microscopy data acquired using a pixelated detector',
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        author = 'Magnus Nord',
-        author_email = 'magnunor@gmail.com',
-        license = 'GPL v3',
-        url = 'http://pixstem.org/',
-        download_url = 'https://gitlab.com/pixstem/pixstem/repository/archive.tar?ref=0.4.0',
-        keywords = [
-            'STEM',
-            'data analysis',
-            'microscopy',
-            ],
-        install_requires = [
-            'scipy',
-            'numpy>=1.13',
-            'h5py',
-            'ipython>=2.0',
-            'matplotlib>=3.1.0',
-            'hyperspy>=1.4',
-            'dask',
-            'tqdm',
-            'scikit-image>=0.15',
-            'scikit-learn',
-            ],
-        package_data={'pixstem': ['tests/test_data/*.hdf5',
-                                  ]},
 )
