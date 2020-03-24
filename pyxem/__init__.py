c79df3b9ed41eb9feb714c7f803b25fa04c39c58
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-03-24 22:05:20 +0000

    move pixstem api into pyxem api

--- pyxem/__init__.py
+++ pyxem/__init__.py
@@ -62,6 +62,15 @@ from .signals.reduced_intensity1d import ReducedIntensity1D
 
 from pyxem.utils.io_utils import load_mib
 
+from pixstem.pixelated_stem_class import LazyPixelatedSTEM
+from pixstem.pixelated_stem_class import PixelatedSTEM
+from pixstem.pixelated_stem_class import (
+        DPCBaseSignal, DPCSignal1D, DPCSignal2D)
+from pixstem.io_tools import load_ps_signal, load_dpc_signal
+import pixstem.radial as radial
+import pixstem.dummy_data as dummy_data
+import pixstem.io_tools as io_tools
+
 from . import release_info
 
 __version__ = release_info.version
