07e05e73a2bb1d415cdc4b2d82982e4cebec541e
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-03-25 15:16:45 +0000

    add back TestVirtualImaging lost by mistake

--- pyxem/tests/test_signals/test_diffraction2d.py
+++ pyxem/tests/test_signals/test_diffraction2d.py
@@ -231,3 +231,69 @@ class TestPolarReprojection:
         assert polar_t_axis.units == "$rad$"
         assert polar_k_axis.name == "k"
         assert polar_k_axis.units == "$rad$"
+
+
+class TestVirtualImaging:
+    # Tests that virtual imaging runs without failure
+
+    @pytest.mark.parametrize("stack", [True, False])
+    def test_plot_integrated_intensity(self, stack, diffraction_pattern):
+        if stack:
+            diffraction_pattern = hs.stack([diffraction_pattern] * 3)
+        roi = hs.roi.CircleROI(3, 3, 5)
+        diffraction_pattern.plot_integrated_intensity(roi)
+
+    def test_get_integrated_intensity(self, diffraction_pattern):
+        roi = hs.roi.CircleROI(3, 3, 5)
+        vi = diffraction_pattern.get_integrated_intensity(roi)
+        assert vi.data.shape == (2, 2)
+        assert vi.axes_manager.signal_dimension == 2
+        assert vi.axes_manager.navigation_dimension == 0
+
+    @pytest.mark.parametrize("out_signal_axes", [None, (0, 1), (1, 2), ("x", "y")])
+    def test_get_integrated_intensity_stack(self, diffraction_pattern, out_signal_axes):
+        s = hs.stack([diffraction_pattern] * 3)
+        s.axes_manager.navigation_axes[0].name = "x"
+        s.axes_manager.navigation_axes[1].name = "y"
+
+        roi = hs.roi.CircleROI(3, 3, 5)
+        vi = s.get_integrated_intensity(roi, out_signal_axes)
+        assert vi.axes_manager.signal_dimension == 2
+        assert vi.axes_manager.navigation_dimension == 1
+        if out_signal_axes == (1, 2):
+            assert vi.data.shape == (2, 3, 2)
+            assert vi.axes_manager.navigation_size == 2
+            assert vi.axes_manager.signal_shape == (2, 3)
+        else:
+            assert vi.data.shape == (3, 2, 2)
+            assert vi.axes_manager.navigation_size == 3
+            assert vi.axes_manager.signal_shape == (2, 2)
+
+    def test_get_integrated_intensity_out_signal_axes(self, diffraction_pattern):
+        s = hs.stack([diffraction_pattern] * 3)
+        roi = hs.roi.CircleROI(3, 3, 5)
+        vi = s.get_integrated_intensity(roi, out_signal_axes=(0, 1, 2))
+        assert vi.axes_manager.signal_dimension == 3
+        assert vi.axes_manager.navigation_dimension == 0
+        assert vi.metadata.General.title == "Integrated intensity"
+        assert (
+            vi.metadata.Diffraction.intergrated_range
+            == "CircleROI(cx=3, cy=3, r=5) of Stack of "
+        )
+
+    def test_get_integrated_intensity_error(
+        self, diffraction_pattern, out_signal_axes=(0, 1, 2)
+    ):
+        roi = hs.roi.CircleROI(3, 3, 5)
+        with pytest.raises(ValueError):
+            _ = diffraction_pattern.get_integrated_intensity(roi, out_signal_axes)
+
+    def test_get_integrated_intensity_linescan(self, diffraction_pattern):
+        s = diffraction_pattern.inav[0, :]
+        s.metadata.General.title = ""
+        roi = hs.roi.CircleROI(3, 3, 5)
+        vi = s.get_integrated_intensity(roi)
+        assert vi.data.shape == (2,)
+        assert vi.axes_manager.signal_dimension == 1
+        assert vi.axes_manager.navigation_dimension == 0
+        assert vi.metadata.Diffraction.intergrated_range == "CircleROI(cx=3, cy=3, r=5)"
