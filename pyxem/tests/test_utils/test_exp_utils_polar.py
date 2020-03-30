6544736b90dabdb2785526a6dcd66b9fa2d0fb34
Carter Francis <csfrancis@wisc.edu>
2020-03-30 10:26:53 -0500

    Correlation method changed to properly deal with masks and wrapping.  Tests added for correlation method.

--- pyxem/tests/test_utils/test_exp_utils_polar.py
+++ pyxem/tests/test_utils/test_exp_utils_polar.py
@@ -19,41 +19,66 @@
 import pytest
 import numpy as np
 
-from pyxem.utils.exp_utils_polar import _correlation, angular_correlation, angular_power, _power
+from pyxem.utils.exp_utils_polar import _correlation
 
 
 class TestCorrelations:
     @pytest.fixture
     def ones_array(self):
-        return np.ones((10,20))
+        return np.ones((10, 20))
+
     @pytest.fixture
     def ones_zero(self):
-        ones = np.ones((10,20))
-        ones[0:20:2,:]=0
+        ones = np.ones((10, 20))
+        ones[0:20:2, :] = 0
+        return ones
+
+    @pytest.fixture
+    def ones_hundred(self):
+        ones = np.ones((10, 20))
+        ones[0:20:2, :] = 100
         return ones
 
     def test_correlation_ones(self, ones_array):
         c = _correlation(ones_array)
-        np.testing.assert_array_equal(c,np.zeros((10,20)))
+        np.testing.assert_array_equal(c, np.zeros((10, 20)))
 
     def test_correlations_axis(self, ones_zero):
         c = _correlation(ones_zero, axis=0, normalize=True)
-        result = np.ones((10,20))
-        result[1::2,:]=-1
+        result = np.ones((10, 20))
+        result[1::2, :] = -1
         np.testing.assert_array_equal(c, result)
         c = _correlation(ones_zero, axis=0, normalize=False)
-        result = np.zeros((10,20))
-        result[0::2,:]=5
+        result = np.zeros((10, 20))
+        result[0::2, :] = 5
         np.testing.assert_array_almost_equal(c, result)
 
-    def test_correlations_(self, ones_zero):
-        c = _correlation(ones_zero, axis=0, normalize=True)
-        result = np.ones((10,20))
-        result[1::2,:]=-1
-        np.testing.assert_array_equal(c, result)
-        c = _correlation(ones_zero, axis=0, normalize=False)
-        result = np.zeros((10,20))
-        result[0::2,:]=5
+    def test_correlations_normalization(self, ones_hundred):
+        c = _correlation(ones_hundred, axis=0, normalize=True)
+        result = np.zeros((10, 20))
+        result[1::2, :] = -.96078816
+        result[0::2, :] = .96078816
+        np.testing.assert_array_almost_equal(c, result)
+        c = _correlation(ones_hundred, axis=1, normalize=True)
+        result = np.zeros((10, 20))
         np.testing.assert_array_almost_equal(c, result)
 
+    def test_correlations_mask(self, ones_hundred):
+        m = np.zeros((10, 20))
+        m[2:4, :] = 1
+        c = _correlation(ones_hundred, axis=0, normalize=True, mask=m)
+        print(c)
+        result = np.zeros((10, 20))
+        result[1::2, :] = -.96078816
+        result[0::2, :] = .96078816
+        np.testing.assert_array_almost_equal(c, result)
 
+    def test_correlations_wrapping(self, ones_hundred):  # Need to do extra checks to assure this is correct
+        m = np.zeros((10, 20))
+        m[2:4, :] = 1
+        c = _correlation(ones_hundred, axis=0, normalize=True, wrap=False)
+        print(c)
+        result = np.zeros((10, 20))
+        result[0::2, :] = 2.26087665
+        result[1::2, :] = -0.93478899
+        np.testing.assert_array_almost_equal(c, result)
--- pyxem/utils/exp_utils_polar.py
+++ pyxem/utils/exp_utils_polar.py
@@ -21,44 +21,50 @@ def _correlation(z, axis=0, mask=None, wrap=True, normalize=True):
     normalize: bool
         Subtract <I(\theta)>^2 and divide by <I(\theta)>^2
     """
-    m = mask
     if wrap is False:
         z_shape = np.shape(z)
         padder = [(0,0)]*len(z_shape)
-        pad = z_shape[axis]//2  #  This will be faster if the length of the axis
+        pad = z_shape[axis]  #  This will be faster if the length of the axis
         # is a power of 2.  Based on the numpy implementation.  Not terribly
         # faster I think..
-        padder[axis]= z_shape[axis] = (pad, pad)
-        slicer = (slice(None),)*len(z_shape)
-        slicer[axis] = slice(pad,-pad)  # creating the proper slices
+        padder[axis] = (pad, pad)
+        slicer = [slice(None),]*len(z_shape)
+        slicer[axis] = slice(0,- 2 * pad)  # creating the proper slices
+        if mask is None:
+            mask = np.zeros(shape=np.shape(z))
+        z = np.pad(z, padder, 'constant')
 
-    if m is not None:
+    if mask is not None or wrap is False:  # Need to scale for wrapping properly
+        m = np.array(mask, dtype=bool)  # make sure it is a boolean array
         # this is to determine how many of the variables were non zero... This is really dumb.  but...
         # it works and I should stop trying to fix it (wreak it)
         mask_boolean = ~ m  # inverting the boolean mask
         if wrap is False:  # padding with zeros to the function along some axis
             m = np.pad(m, padder,'constant')  # all the zeros are masked (should account for padding
             #  when normalized.
+            mask_boolean = np.pad(mask_boolean, padder,'constant')
         mask_fft = np.fft.fft(mask_boolean, axis=axis)
-        number_unmasked = np.fft.ifft(mask_fft*np.conjugate(mask_fft), axis=axis.real)
+        number_unmasked = np.fft.ifft(mask_fft*np.conjugate(mask_fft), axis=axis).real
         number_unmasked[number_unmasked < 1] = 1  # get rid of divide by zero error for completely masked rows
         z[m] = 0
 
     # fast method uses a FFT and is a process which is O(n) = n log(n)
-    if wrap is False:
-        z = np.pad(z, padder, 'constant')
     I_fft = np.fft.fft(z, axis=axis)
     a = np.fft.ifft(I_fft * np.conjugate(I_fft), axis=axis).real
 
-    if m is not None:
-        a = np.multiply(np.divide(a, np.transpose(number_unmasked)), np.shape(z)[axis])
+    if mask is not None:
+        print("The number unmasked",number_unmasked)
+        print(np.shape(z)[0])
+        a = np.multiply(np.divide(a, number_unmasked), np.shape(z)[0])
 
-    if normalize:
+    if normalize:  # simplified way to calculate the normalization
         row_mean = np.mean(a, axis=axis)
         row_mean[row_mean == 0] = 1
         row_mean = np.expand_dims(row_mean, axis=axis)
         a = np.divide(np.subtract(a, row_mean), row_mean)
+
     if wrap is False:
+        print(slicer)
         a = a[slicer]
     return a
 
