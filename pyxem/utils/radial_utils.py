8cab8af066b26979e752cda96aaa5565233fd8fe
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-04-14 21:20:55 +0100

    pyxemify radial_utils

--- pyxem/utils/radial_utils.py
+++ pyxem/utils/radial_utils.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2020 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
 import math
 import numpy as np
 import numpy.linalg as la
@@ -6,6 +24,7 @@ from hyperspy.components1d import Polynomial, Gaussian
 from hyperspy.signals import Signal2D
 from hyperspy.utils.markers import point, line_segment
 from hyperspy.misc.utils import isiterable
+
 import pyxem.utils.pixelated_stem_tools as pst
 
 
