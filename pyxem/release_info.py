e98ad379b96ecd956a6d9f4237e095928826cdfe
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-04-14 18:01:30 +0100

    v0.12.0dev

--- pyxem/release_info.py
+++ pyxem/release_info.py
@@ -1,5 +1,5 @@
 name = "pyxem"
-version = "0.12.0"
+version = "0.12.0dev"
 author = "Duncan Johnstone, Phillip Crout, Magnus Nord"
 copyright = "Copyright 2017-2020, The pyXem Developers"
 credits = [
