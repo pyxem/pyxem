5665ac27a335da2927b2de048aed23984333c99b
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-04-14 21:27:09 +0100

    add copyright statement

--- pyxem/utils/dask_tools.py
+++ pyxem/utils/dask_tools.py
@@ -1,4 +1,22 @@
-pxm.Diffraction2Dpxm.dummy_data.dummy_datapyxem.utils.dask_toolsimport copy
+# -*- coding: utf-8 -*-
+# Copyright 2017-2020 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+import copy
 import numpy as np
 import dask.array as da
 from skimage.feature import match_template, blob_dog, blob_log
