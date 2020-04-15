8fab2940a41d2b24b4dd6d883eed6cf9d268fd4d
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-04-15 08:05:36 +0100

    import numpy and make just 1 find_peaks

--- pyxem/utils/dpc_utils.py
+++ pyxem/utils/dpc_utils.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
 
+import numpy as np
 import scipy.constants as sc
 import pyxem.utils.diffraction_tools as dt
 
