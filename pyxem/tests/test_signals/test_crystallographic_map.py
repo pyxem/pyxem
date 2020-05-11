00bc1716c2a2a2417c47f9b1f39d48424f89dc36
norskie <endrejac@stud.ntnu.no>
2020-05-11 18:57:32 +0900

    Added a test

--- pyxem/tests/test_signals/test_crystallographic_map.py
+++ pyxem/tests/test_signals/test_crystallographic_map.py
@@ -21,6 +21,9 @@ import pytest
 from pyxem.signals.crystallographic_map import CrystallographicMap
 from pyxem.signals.crystallographic_map import load_mtex_map
 from pyxem.signals.crystallographic_map import _distance_from_fixed_angle
+
+from diffsims.libraries.diffraction_library import DiffractionLibrary
+
 from transforms3d.euler import euler2quat, quat2axangle
 from transforms3d.quaternions import qmult, qinverse
 import os
@@ -46,6 +49,59 @@ def get_distance_between_two_angles_longform(angle_1, angle_2):
     axis, angle = quat2axangle(q_from_mode)
     return np.rad2deg(angle)
 
+@pytest.fixture()
+def test_plot_phase_map():
+    """
+    Generates a CrystallographicMap with two phases and a corresponding DiffractionLibraryself
+    and tests the CrystallographicMap member function plot_phase_map.
+    """
+    base = np.empty((4, 3), dtype="object")
+    base[0] = [
+        0,
+        np.array([5, 17, 6]),
+        {
+            "correlation": 3e-17,
+            "orientation_reliability": 0.5,
+            "phase_reliability": 0.6,
+        },
+        ]
+    base[1] = [
+        1,
+        np.array([6, 17, 6]),
+        {
+            "correlation": 2e-17,
+            "orientation_reliability": 0.4,
+            "phase_reliability": 0.7,
+        },
+        ]
+    base[2] = [
+        1,
+        np.array([12, 3, 6]),
+        {
+            "correlation": 4e-17,
+            "orientation_reliability": 0.3,
+            "phase_reliability": 0.1,
+        },
+        ]
+    base[3] = [
+        0,
+        np.array([12, 3, 5]),
+        {
+            "correlation": 8e-16,
+            "orientation_reliability": 0.2,
+            "phase_reliability": 0.8,
+        },
+        ]
+
+    crystal_map = CrystallographicMap(base.reshape((2, 2, 3)))
+    crystal_map.method = "template_matching"
+
+    #Create DiffractionLibrary with two entries
+    lib = DiffractionLibrary()
+    lib['Al'] = ''
+    lib['Si'] = ''
+
+    crystal_map.plot_phase_map(lib)
 
 @pytest.fixture()
 def sp_cryst_map():
