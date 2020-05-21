ef50f86264511e3609a0b47d5ffd72a2bf47357b
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-05-21 14:45:51 +0100

    change atol

--- pyxem/tests/pixstem_tests/test_dpc_signal_class.py
+++ pyxem/tests/pixstem_tests/test_dpc_signal_class.py
@@ -69,8 +69,8 @@ class TestDpcSignal2dCorrectRamp:
         s_y = DPCSignal2D(data_y)
         s_x_corr = s_x.correct_ramp(corner_size=0.05)
         s_y_corr = s_y.correct_ramp(corner_size=0.05)
-        assert_allclose(s_x_corr.data, np.zeros_like(data_x), atol=1e-6)
-        assert_allclose(s_y_corr.data, np.zeros_like(data_y), atol=1e-6)
+        assert_allclose(s_x_corr.data, np.zeros_like(data_x), atol=1e-8)
+        assert_allclose(s_y_corr.data, np.zeros_like(data_y), atol=1e-8)
 
         data_xy = np.swapaxes(np.dstack((array_x, array_y)), 0, 2).astype("float64")
         data_yx = np.swapaxes(np.dstack((array_y, array_x)), 0, 2).astype("float64")
@@ -78,8 +78,8 @@ class TestDpcSignal2dCorrectRamp:
         s_yx = DPCSignal2D(data_yx)
         s_xy_corr = s_xy.correct_ramp(corner_size=0.05)
         s_yx_corr = s_yx.correct_ramp(corner_size=0.05)
-        assert_allclose(s_xy_corr.data, np.zeros_like(data_xy), atol=1e-8)
-        assert_allclose(s_yx_corr.data, np.zeros_like(data_yx), atol=1e-8)
+        assert_allclose(s_xy_corr.data, np.zeros_like(data_xy), atol=1e-6)
+        assert_allclose(s_yx_corr.data, np.zeros_like(data_yx), atol=1e-6)
 
         data_tilt = np.swapaxes(
             np.dstack((array_x + array_y, np.fliplr(array_x) + array_y)), 0, 2
