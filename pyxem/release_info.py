338d3122467769ad665febf01e073de5e236f4ca
phillip <pc494@cam.ac.uk>
2020-10-04 19:19:22 +0100

    version to 0.13.0-dev

--- pyxem/release_info.py
+++ pyxem/release_info.py
@@ -1,5 +1,5 @@
 name = "pyxem"
-version = "0.12.2"
+version = "0.13.0-dev"
 author = "Duncan Johnstone, Phillip Crout, Magnus Nord"
 copyright = "Copyright 2016-2020, The pyXem Developers"
 credits = [
