f807f07a6b2ff7dffafde58d3b62ced18a1431e2
phillip <pc494@cam.ac.uk>
2020-12-12 17:46:22 +0000

    Update to modern scikit image style for phasexc

--- pyxem/generators/subpixelrefinement_generator.py
+++ pyxem/generators/subpixelrefinement_generator.py
@@ -20,7 +20,7 @@
 
 import numpy as np
 
-from skimage.feature import register_translation
+from skimage.registeration import phase_correlation
 from skimage import draw
 
 from pyxem.signals.diffraction_vectors import DiffractionVectors
@@ -164,7 +164,7 @@ def _conventional_xc(exp_disc, sim_disc, upsample_factor):
 
     """
 
-    shifts, error, _ = register_translation(exp_disc, sim_disc, upsample_factor)
+    shifts, error, _ = phase_correlation(exp_disc, sim_disc, upsample_factor)
     shifts = np.flip(shifts)  # to comply with hyperspy conventions - see issue#490
     return shifts
 
