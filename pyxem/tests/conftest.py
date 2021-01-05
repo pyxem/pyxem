2f1aa775180ab061786940b80b74c5080bc6d3a2
phillip <pc494@cam.ac.uk>
2021-01-05 18:04:03 +0000

    Pragma no cover the runslow option in conftest

--- pyxem/tests/conftest.py
+++ pyxem/tests/conftest.py
@@ -47,10 +47,11 @@ def pytest_collection_modifyitems(config, items):
     if config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
         return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+    else: #pragma: no cover
+        skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
 
 # End of the code lift, it's regular code from here on out
 
