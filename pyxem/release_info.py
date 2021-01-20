606d0f805f276dcd45af4384a713b1cd9f02e4e9
Phillip Crout <32455330+pc494@users.noreply.github.com>
2021-01-20 15:11:42 +0000

    bumps version to dev

--- pyxem/release_info.py
+++ pyxem/release_info.py
@@ -1,5 +1,5 @@
 name = "pyxem"
-version = "0.13.0"
+version = "0.13.1-dev"
 author = "Duncan Johnstone, Phillip Crout, Magnus Nord"
 copyright = "Copyright 2016-2021, The pyxem developers"
 credits = [
