4d915720247f8e8ed37e7ef3fadba46d16433691
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-01-12 17:01:31 +0100

    Remove unnecessary version check
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- pyxem/utils/segment_utils.py
+++ pyxem/utils/segment_utils.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
 
-from packaging.version import Version
-
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.ma import masked_where
@@ -27,13 +25,8 @@ from scipy.signal import convolve2d
 from skimage.feature import peak_local_max
 from skimage.filters import sobel, threshold_li
 from skimage.morphology import disk
+from skimage.segmentation import watershed
 from sklearn.cluster import DBSCAN
-
-from skimage import __version__ as skimage_version_str
-if Version(skimage_version_str) < Version("0.19"):
-    from skimage.morphology import watershed
-else:
-    from skimage.segmentation import watershed
 
 
 def norm_cross_corr(image, template):
