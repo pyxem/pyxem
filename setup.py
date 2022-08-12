b2272bad659e5aaea0ed7389b6dd4f331709ed4f
phillip <pc494@cam.ac.uk>
2022-08-12 13:33:06 +0100

    Acknowledge py310 in setup

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
