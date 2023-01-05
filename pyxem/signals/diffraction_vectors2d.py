793dc54610dc9c594359c56a00d7a9cc59c28b74
cssfrancis <csfrancis@wisc.edu>
2023-01-05 12:12:41 -0600

    NewFeature: Split out DiffractionVector1D and DiffractionVector2D to better align with hyperspy Signal definition.

---
+++ pyxem/signals/diffraction_vectors2d.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2023 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from hyperspy.signals import Signal2D
+
+class DiffractionVectors2D(Signal2D):
+    """Crystallographic mapping results containing the best matching crystal
+    phase and orientation at each navigation position with associated metrics.
+
+    Attributes
+    ----------
+    cartesian : np.array()
+        Array of 3-vectors describing Cartesian coordinates associated with
+        each diffraction vector.
+    hkls : np.array()
+        Array of Miller indices associated with each diffraction vector
+        following indexation.
+    """
+
+    _signal_dimension = 2
+
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        if self.axes_manager.signal_dimension != 2:
+            self.axes_manager.set_signal_dimension(2)
