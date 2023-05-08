af1e59b22f8415460bfcc601631942b735a7d122
Cssfrancis <csfrancis@wisc.edu>
2023-05-07 20:13:39 -0500

    Release: Updated Release info for 0.15.1

--- pyxem/release_info.py
+++ pyxem/release_info.py
@@ -1,5 +1,5 @@
 name = "pyxem"
-version = "0.15.0"
+version = "0.15.1"
 author = "Duncan Johnstone, Phillip Crout, Magnus Nord"
 copyright = "Copyright 2016-2023, The pyxem developers"
 # Contributors listed by original commiter, maintainers, then other
