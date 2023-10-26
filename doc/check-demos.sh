2d8ff94c78e26065a16e3a9bb26fc0b16a156c96
Carter Francis <csfrancis@wisc.edu>
2023-10-26 08:39:50 -0500

    Refactor: Clean up make file for demos

--- doc/check-demos.sh
+++ doc/check-demos.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
 # Check that all demos are working
-if [ -d "doc/tutorials/pyxem-demos" ]; then
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+echo "Script directory: $SCRIPT_DIR"
+echo "$SCRIPT_DIR/tutorials/pyxem-demos"
+if [ -d "$SCRIPT_DIR/tutorials/pyxem-demos" ]; then
 		echo "The doc/tutorials/pyxem-demos folder does exist"
 	else
-		echo "The doc/tutorials/pyxem-demos folder does not exist"
+		echo "The ./tutorials/pyxem-demos folder does not exist"
 		echo "Fetching the pyxem-demos from the repository"
-		(cd "doc/tutorials" || exit
-		git clone https://github.com/pyxem/pyxem-demos.git)
+		git clone https://github.com/pyxem/pyxem-demos.git "$SCRIPT_DIR/tutorials/pyxem-demos"
 	fi
