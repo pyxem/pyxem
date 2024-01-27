1202f910db38891cbd385418146dd54003b1b547
pc494 <phillipcrout@outlook.com>
2024-01-27 09:49:32 +0000

    [chore] Forcing black 24.1.0 on dev+precommit

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ extra_feature_requirements = {
         "coveralls  >= 1.10",
         "coverage   >= 5.0",
     ],
-    "dev": ["black", "pre-commit >=1.16"],
+    "dev": ["black ~= 24.1", "pre-commit >=1.16"],
     "gpu": ["cupy >= 9.0.0"],
     "dask": ["dask-image", "distributed"],
 }
