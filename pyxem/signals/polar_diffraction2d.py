a7cedeb4053731a78438c18d9a5e03993c1f71a4
Carter Francis <csfrancis@wisc.edu>
2024-02-08 05:55:50 -0600

    Refactor: Moved orientation mapping to polar_diffraction2d.py

--- pyxem/signals/polar_diffraction2d.py
+++ pyxem/signals/polar_diffraction2d.py
@@ -23,6 +23,10 @@ from hyperspy._signals.lazy import LazySignal
 from pyxem.signals.common_diffraction import CommonDiffraction
 from pyxem.utils._correlations import _correlation, _power, _pearson_correlation
 from pyxem.utils._deprecated import deprecated
+from pyxem.utils.indexation_utils import (
+    _mixed_matching_lib_to_polar,
+    _get_integrated_polar_templates,
+)
 
 from pyxem.utils._background_subtraction import (
     _polar_subtract_radial_median,
@@ -303,6 +307,51 @@ class PolarDiffraction2D(CommonDiffraction, Signal2D):
             output_signal_size=self.axes_manager._signal_shape_in_array,
             **kwargs,
         )
+
+    def get_orientation(
+        self, simulation, n_keep=1, frac_keep=0.1, n_best=1, normalize_templates=True
+    ):
+        """Match the orientation with some simulated diffraction patterns using
+        an accelerated orientation mapping algorithm.
+
+        The details of the algorithm are described in the paper:
+        "Free, flexible and fast: Orientation mapping using the multi-core and
+         GPU-accelerated template matching capabilities in the python-based open
+         source 4D-STEM analysis toolbox Pyxem"
+
+
+        Parameters
+        ----------
+        simulation : DiffractionSimulation
+            The diffraction simulation object to use for indexing.
+
+        Returns
+        -------
+        orientation : BaseSignal
+            A signal with the orientation at each navigation position.
+        """
+        (
+            r_templates,
+            theta_templates,
+            intensities_templates,
+        ) = simulation.get_polar_templates()
+        radius = self.axes_manager.signal_axes[0].size  # number radial pixels
+        integrated_templates = _get_integrated_polar_templates(
+            radius, r_templates, intensities_templates, normalize_templates
+        )
+        orientation = self.map(
+            _mixed_matching_lib_to_polar,
+            integrated_templates=integrated_templates,
+            r_templates=r_templates,
+            theta_templates=theta_templates,
+            intensities_templates=intensities_templates,
+            n_keep=n_keep,
+            frac_keep=frac_keep,
+            n_best=n_best,
+            inplace=False,
+        )
+
+        return orientation
 
 
 class LazyPolarDiffraction2D(LazySignal, PolarDiffraction2D):
