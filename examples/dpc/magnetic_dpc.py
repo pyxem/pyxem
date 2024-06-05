86745381ccd92c879b71a3cd1e76474563ea1c53
Magnus Nord <magnunor@gmail.com>
2024-06-05 16:53:31 +0200

    Improve the STEM-DPC example

--- examples/dpc/magnetic_dpc.py
+++ examples/dpc/magnetic_dpc.py
@@ -14,11 +14,9 @@ import pyxem as pxm
 
 s = pxm.data.simulated_stripes()
 
-beam_pos = s.get_direct_beam_position(
+s_beam_pos = s.get_direct_beam_position(
     method="center_of_mass", half_square_width=20
 )  # Just the center of the DP
-
-beam_pos.plot()
 
 # %%
 # Visualizing the Shifts
@@ -26,8 +24,31 @@ beam_pos.plot()
 # The shifts can be visualized using the :meth:`plot` method which shows both the
 # x and y shifts in a single plot.
 
-s.plot()
+s_beam_pos.plot()
+
+# %%
+# Visualizing the Shifts as color signal
+# --------------------------------------
+# To plot the shifts in one signal, with the color showing the magnitude and direction.
+
+s_color = s.get_color_signal()
+s_color.plot()
 
 # %%
 # Plotting Magnitude and Direction
 # --------------------------------
+# Since each probe position is a vector, we can plot the magnitude and phase (direction).
+
+s_magnitude = s.get_magnitude_signal()
+s_magnitude.plot()
+
+s_phase = s.get_phase_signal()
+s_phase.plot()
+
+# %%
+# Plotting the bivariate histogram
+# --------------------------------
+# The shifts vectors can also be visualized as a histogram
+
+s_hist = s_beam_pos.get_bivariate_histogram()
+s_hist.plot()
