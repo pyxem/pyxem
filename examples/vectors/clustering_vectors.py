1575fd758b01789fd411372e78594d298511b085
Carter Francis <csfrancis@wisc.edu>
2024-08-12 10:11:27 -0500

    Update: Update clustering_vectors.py to use new simulated dataset.

--- examples/vectors/clustering_vectors.py
+++ examples/vectors/clustering_vectors.py
@@ -15,9 +15,8 @@ from sklearn.cluster import DBSCAN
 # Getting the vectors for some dataset
 # s = pxm.data.mgo_nanocrystals(allow_download=True)
 s = pxm.data.simulated_overlap()
-s.data[s.data < 120] = 1
 s.filter(gaussian_filter, sigma=(0.5, 0.5, 0, 0), inplace=True)  # only in real space
-s.template_match_disk(disk_r=3, subtract_min=False, inplace=True)
+s.template_match_disk(disk_r=5, subtract_min=False, inplace=True)
 vectors = s.get_diffraction_vectors(threshold_abs=0.5, min_distance=3)
 
 # Now we can convert the vectors into a 2D array of rows/columns
@@ -30,11 +29,11 @@ scan = DBSCAN(eps=1.0, min_samples=2)
 # ======================
 # It is very important that we first normalize the real and reciprocal space distances
 # The column scale factors map the real space and reciprocal space distances to the same scale
-# Here this means that the clustering algorithm operates on 10 nm in real space and .1 nm^-1 in
+# Here this means that the clustering algorithm operates on 4 nm in real space and .1 nm^-1 in
 # reciprocal space based on the units for the vectors.
 clustered = flat_vectors.cluster(
     scan,
-    column_scale_factors=[10, 10, 0.05, 0.05],
+    column_scale_factors=[2, 2, 0.05, 0.05],
     columns=[0, 1, 2, 3],
     min_vectors=40,
 )
@@ -57,7 +56,7 @@ plt.scatter(vect[:, 1], vect[:, 0])
 
 # %%
 
-clusterer = DBSCAN(min_samples=2, eps=20)
+clusterer = DBSCAN(min_samples=2, eps=4)
 
 clustered2 = clustered.cluster_labeled_vectors(method=clusterer)
 m, p = clustered2.to_markers(s, alpha=0.8, get_polygons=True)
@@ -66,13 +65,16 @@ m, p = clustered2.to_markers(s, alpha=0.8, get_polygons=True)
 # Visualizing the Clustering
 # ==========================
 #
-# This clustering is decent.  It shows that there might be some small tilt boundaries in the data
-# which segment some of the nano-crystals into different clusters.  It also shows the effect of using
-# a phosphor screen which has some pretty severe after glow.  This results in a smearing of the
-# features and elongated clusters along the scan direction.
+# This clustering is works pretty good after the second step.  We can interact with the results as well in order
+# to see regions where the clustering doesn't work quite as well!
+
+# %%
+
+m, p = clustered2.to_markers(
+    s, alpha=0.8, get_polygons=True, facecolor="none", sizes=(30,), lw=5
+)
+s.axes_manager.indices = (45, 45)
 s.plot()
 s.add_marker(m)
 s.add_marker(p, plot_on_signal=False)
-# %%
-
-# sphinx_gallery_thumbnail_number = 3
+# sphinx_gallery_thumbnail_number = 5
