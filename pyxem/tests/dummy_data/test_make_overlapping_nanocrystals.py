f14a61da6e8d8fe37e6efb074643538990dd41c6
Carter Francis <csfrancis@wisc.edu>
2024-08-12 09:25:07 -0500

    New Feature: Add functions to simulate overlapping nano-crystals to use in Examples.

---
+++ pyxem/tests/dummy_data/test_make_overlapping_nanocrystals.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2024 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyxem.data.dummy_data import CrystalSTEMSimulation
+
+from pyxem.data import simulated_overlap
+
+
+class TestMakeOverlapData:
+    def test_init(self):
+        s = simulated_overlap()
