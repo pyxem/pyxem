deca4d20fab2a6f85cce3e66ef1a3d43b2e3ec17
Carter Francis <csfrancis@wisc.edu>
2025-05-29 09:19:39 -0500

    Calibration: Improve Calibration workflows with unit switching

--- pyxem/tests/utils/test_calibration_utils.py
+++ pyxem/tests/utils/test_calibration_utils.py
@@ -93,6 +93,57 @@ class TestCalibrationClass:
         assert calibration.scale == [0.01, 0.01]
         assert calibration.flat_ewald is True
 
+    def test_set_camera_length_from_calibration(self, calibration):
+        calibration.scale = 0.156
+        calibration.units = "nm^-1"
+        calibration.beam_energy = 200
+        calibration.pixel_size = 15e-6  # 15 um
+        calibration.set_camera_length_from_calibration()
+        assert calibration.camera_length == pytest.approx(0.038, rel=1e-2)
+
+    @pytest.mark.parametrize("units", ["nm^-1", "A^-1", "px", "mrad"])
+    def test_change_signal_units(self, calibration, units):
+        calibration.scale = 0.156
+        calibration.units = "nm^-1"
+        calibration.beam_energy = 200
+        calibration.pixel_size = 15e-6  # 15 um
+        calibration.change_signal_units(units)
+        if units == "nm^-1":
+            np.testing.assert_almost_equal(calibration.scale, [0.156, 0.156])
+        elif units == "A^-1":
+            np.testing.assert_almost_equal(calibration.scale, [0.0156, 0.0156])
+        elif units == "px":
+            np.testing.assert_almost_equal(calibration.scale, [1, 1])
+        calibration.change_signal_units("nm^-1")
+        np.testing.assert_almost_equal(calibration.scale, [0.156, 0.156])
+
+    @pytest.mark.parametrize("units", ["nm^-1", "A^-1", "px", "mrad"])
+    def test_change_signal_units_rebin(self, calibration, units):
+        s = calibration.signal
+        s.calibration.scale = 0.156
+        s.calibration.units = "nm^-1"
+        s.calibration.beam_energy = 200
+        s.calibration.pixel_size = 15e-6  # 15 um
+
+        s_rebin = s.calibration.signal.rebin(scale=(2, 1))
+        assert s_rebin.calibration.pixel_size == [30e-6, 15e-6]  # 15 um and 30 um
+        assert s_rebin.axes_manager[0].scale == 0.156 * 2
+        np.testing.assert_almost_equal(s_rebin.calibration.scale, [0.156 * 2, 0.156])
+        s_rebin.calibration.change_signal_units(units)
+        assert s_rebin.calibration.camera_length == pytest.approx(0.038, rel=1e-2)
+        if units == "nm^-1":
+            np.testing.assert_almost_equal(
+                s_rebin.calibration.scale, [0.156 * 2, 0.156]
+            )
+        elif units == "A^-1":
+            np.testing.assert_almost_equal(
+                s_rebin.calibration.scale, [0.0156 * 2, 0.0156]
+            )
+        elif units == "px":
+            np.testing.assert_almost_equal(s_rebin.calibration.scale, [1, 1])
+        s_rebin.calibration.change_signal_units("nm^-1")
+        np.testing.assert_almost_equal(s_rebin.calibration.scale, [0.156 * 2, 0.156])
+
     def test_set_failure(self, calibration):
         assert calibration.wavelength is None
         assert calibration.beam_energy is None
