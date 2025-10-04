465a7e14de240b53b5d71925c400627e100701f4
Eric Prestat <eric.prestat@gmail.com>
2025-10-04 09:54:16 +0900

    Specify output dtype and shape to avoid unnecessary calculation

--- pyxem/signals/diffraction2d.py
+++ pyxem/signals/diffraction2d.py
@@ -21,7 +21,7 @@ from typing import Optional, Sequence, Tuple, TYPE_CHECKING, Union
 from pyxem.utils._typing import Degrees, Number
 
 import numpy as np
-import scipy.ndimage as ndi
+from scipy.ndimage import rotate
 from skimage import morphology
 import dask.array as da
 from dask.diagnostics import ProgressBar
@@ -186,15 +186,11 @@ class Diffraction2D(CommonDiffraction, Signal2D):
                 convert_affine_to_transform, shape=shape, inplace=False
             )
 
-        if not keep_dtype:
-            out_dtype = float
-        else:
-            out_dtype = self.data.dtype
-
         return self.map(
             apply_transformation,
             transformation=transformation,
-            output_dtype=out_dtype,
+            output_dtype=self.data.dtype if keep_dtype else float,
+            output_signal_size=self.axes_manager.signal_shape[::-1],
             order=order,
             keep_dtype=keep_dtype,
             inplace=inplace,
@@ -336,12 +332,14 @@ class Diffraction2D(CommonDiffraction, Signal2D):
 
         """
         s_rotated = self.map(
-            ndi.rotate,
+            rotate,
             ragged=False,
             angle=-angle,
             reshape=False,
             inplace=False,
             show_progressbar=show_progressbar,
+            output_dtype=self.data.dtype,
+            output_signal_size=self.axes_manager.signal_shape[::-1],
         )
         if self._lazy:
             s_rotated.compute(show_progressbar=show_progressbar)
@@ -544,7 +542,12 @@ class Diffraction2D(CommonDiffraction, Signal2D):
         subtraction_function = method_dict[method]
 
         return self.map(
-            subtraction_function, inplace=inplace, silence_warnings=True, **kwargs
+            subtraction_function,
+            inplace=inplace,
+            output_dtype=kwargs.pop("output_dtype", self.data.dtype),
+            output_signal_size=self.axes_manager.signal_shape[::-1],
+            silence_warnings=True,
+            **kwargs,
         )
 
     @deprecated_argument(
@@ -666,6 +669,8 @@ class Diffraction2D(CommonDiffraction, Signal2D):
             threshold_multiplier=threshold_multiplier,
             mask=mask,
             inplace=inplace,
+            output_dtype=bool,
+            output_signal_size=self.axes_manager.signal_shape[::-1],
             **kwargs,
         )
 
@@ -711,7 +716,13 @@ class Diffraction2D(CommonDiffraction, Signal2D):
         find_hot_pixels
 
         """
-        return self.map(remove_bad_pixels, bad_pixels=bad_pixel_array, **kwargs)
+        return self.map(
+            remove_bad_pixels,
+            bad_pixels=bad_pixel_array,
+            output_dtype=kwargs.pop("output_dtype", self.data.dtype),
+            output_signal_size=self.axes_manager.signal_shape[::-1],
+            **kwargs,
+        )
 
     """ Direct beam and peak finding tools """
 
@@ -1147,15 +1158,16 @@ class Diffraction2D(CommonDiffraction, Signal2D):
             x, y, r = mask
             im_x, im_y = self.axes_manager.signal_shape
             mask = pst._make_circular_mask(x, y, im_x, im_y, r)
-        s_out = self.map(
+        return self.map(
             function=pst._threshold_and_mask_single_frame,
             ragged=False,
             inplace=False,
             show_progressbar=show_progressbar,
             threshold=threshold,
             mask=mask,
+            output_dtype=self.data.dtype,
+            output_signal_size=self.axes_manager.signal_shape[::-1],
         )
-        return s_out
 
     @deprecated_argument(
         name="lazy_result", alternative="lazy_output", since="0.15.0", removal="1.0.0"
